aa943d4839a671cc13a9b8780421f5abbc611f78
Brian Warner <warner@lothar.com>
2006-12-05 01:29:26 -0700

    add distutils-based packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+#from setuptools import setup, find_packages
+from distutils.core import setup
+
+setup(
+    name="AllMyData",
+    version="0.0.1",
+    #packages=find_packages('.'),
+    packages=["allmydata"],
+    package_data={ 'allmydata': ['web/*.xhtml'] },
+    description="AllMyData (tahoe2)",
+    )
+
