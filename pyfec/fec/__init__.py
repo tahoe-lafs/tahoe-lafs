3a2056a16fb9c6593f4bbfdf98283eef63c1b76a
Zooko O'Whielacronx <zooko@zooko.com>
2007-04-11 10:49:33 -0700

    give it a version number -- v0.9 I intend to bump it to 1.0 after adding the cmdline tools.

--- pyfec/fec/__init__.py
+++ pyfec/fec/__init__.py
@@ -1,3 +1,21 @@
+"""
+pyfec -- fast forward error correction library with Python interface
+
+maintainer web site: U{http://zooko.com/}
+
+pyfec web site: U{http://www.allmydata.com/source/pyfec}
+"""
+
+from util.version import Version
+
+# For an explanation of what the parts of the version string mean,
+# please see pyutil.version.
+__version__ = Version("0.9.0-0-STABLE")
+
+# Please put a URL or other note here which shows where to get the branch of
+# development from which this version grew.
+__sources__ = ["http://www.allmydata.com/source/pyfec",]
+
 from _fec import Encoder, Decoder, Error
 import filefec
 
