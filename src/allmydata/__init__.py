20451e43b5a2dc8fc12d134f4688b9eb846ddd02
Zooko O'Whielacronx <zooko@zooko.com>
2007-04-19 13:47:59 -0700

    assign version number tahoe 0.1.0-0-UNSTABLE

---
+++ src/allmydata/__init__.py
@@ -0,0 +1,18 @@
+"""
+Decentralized storage mesh.
+
+maintainer web site: U{http://allmydata.com/}
+
+community web site: U{http://allmydata.org/}
+"""
+
+from util.version import Version
+
+# For an explanation of what the parts of the version string mean,
+# please see pyutil.version.
+__version__ = Version("0.1.0-0-UNSTABLE")
+
+# Please put a URL or other note here which shows where to get the branch of
+# development from which this version grew.
+__sources__ = ["http://allmydata.org/source/tahoe",]
+
