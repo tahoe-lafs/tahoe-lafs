0bd9afdfb85558b46543450ef9937ba045b42bf4
Zooko O'Whielacronx <zooko@zooko.com>
2007-04-21 07:49:08 -0700

    zfec: make "./setup.py test" do the right thing

--- src/zfec/setup.py
+++ src/zfec/setup.py
@@ -70,4 +70,5 @@ setup(name='zfec',
       classifiers=trove_classifiers,
       entry_points = { 'console_scripts': [ 'zfec = zfec.cmdline.zfec:main', 'zunfec = zfec.cmdline.zunfec:main' ] },
       ext_modules=[Extension('_fec', ['zfec/fec.c', 'zfec/_fecmodule.c',], extra_link_args=extra_link_args, extra_compile_args=extra_compile_args, undef_macros=undef_macros),],
+      test_suite="zfec.test.test_zfec",
       )
