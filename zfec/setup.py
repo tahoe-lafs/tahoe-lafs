7cf27f9f1d50a49ae0fe4080ea0d4e6ddefc5c64
Zooko O'Whielacronx <zooko@zooko.com>
2007-04-20 19:10:21 -0700

    zfec: version 1.0.0a5-1-STABLE

--- zfec/setup.py
+++ zfec/setup.py
@@ -58,7 +58,7 @@ trove_classifiers=[
     ]
 
 setup(name='zfec',
-      version='1.0.0a4',
+      version='1.0.0a5',
       summary='a fast erasure code with command-line, C, and Python interfaces',
       description='Fast, portable, programmable erasure coding a.k.a. "forward error correction": the generation of redundant blocks of information such that if some blocks are lost then the original data can be recovered from the remaining blocks.',
       author='Zooko O\'Whielacronx',
