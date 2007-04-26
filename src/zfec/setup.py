e11b2c796b9f912e689f4c691a4f1cb0f6a23865
Brian Warner <warner@allmydata.com>
2007-04-25 18:09:03 -0700

    zfec: setup.py: switch back to distutils, since setuptools support isn't complete yet

--- src/zfec/setup.py
+++ src/zfec/setup.py
@@ -26,7 +26,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from setuptools import Extension, setup
+from distutils.core import Extension, setup
 
 DEBUGMODE=False
 # DEBUGMODE=True
