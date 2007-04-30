b027ee1489ec331438ab70ce7fa659a236167960
Zooko O'Whielacronx <zooko@zooko.com>
2007-04-30 10:00:07 -0700

    v0.1.4b2-0-UNSTABLE

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ trove_classifiers=[
     ]
 
 setup(name='allmydata-tahoe',
-      version='0.1.3',
+      version='0.1.4b2',
       description='secure, distributed storage mesh',
       long_description="""Welcome to the AllMyData "tahoe" project. This project implements a
 secure, distributed, fault-tolerant storage mesh.
