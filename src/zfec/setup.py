d198b558462feaebaec2581570685b18bb5c436a
Zooko O'Whielacronx <zooko@zooko.com>
2007-05-02 15:25:41 -0700

    zfec: v1.0.0

--- src/zfec/setup.py
+++ src/zfec/setup.py
@@ -47,9 +47,10 @@ if DEBUGMODE:
     undef_macros.append('NDEBUG')
 
 trove_classifiers=[
-    "Development Status :: 4 - Beta", 
+    "Development Status :: 5 - Production/Stable",
     "Environment :: Console",
     "License :: OSI Approved :: GNU General Public License (GPL)", 
+    "License :: DFSG approved",
     "Intended Audience :: Developers", 
     "Intended Audience :: End Users/Desktop",
     "Intended Audience :: System Administrators",
@@ -76,7 +77,7 @@ trove_classifiers=[
     ]
 
 setup(name='zfec',
-      version='1.0.0b3',
+      version='1.0.0',
       description='a fast erasure code with command-line, C, and Python interfaces',
       long_description='Fast, portable, programmable erasure coding a.k.a. "forward error correction": the generation of redundant blocks of information such that if some blocks are lost then the original data can be recovered from the remaining blocks.',
       author='Zooko O\'Whielacronx',
