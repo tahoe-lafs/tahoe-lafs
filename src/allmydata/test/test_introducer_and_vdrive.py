2d210293677fc9de01e23f50a0dda5b6853a223c
Brian Warner <warner@allmydata.com>
2007-05-23 17:37:20 -0700

    test_introducer_and_vdrive: remove the assumption that startService returns a Deferred

--- src/allmydata/test/test_introducer_and_vdrive.py
+++ src/allmydata/test/test_introducer_and_vdrive.py
@@ -1,6 +1,6 @@
 
 from twisted.trial import unittest
-from foolscap.eventual import flushEventualQueue
+from foolscap.eventual import fireEventually, flushEventualQueue
 
 from allmydata import introducer_and_vdrive
 from allmydata.util import testutil
@@ -8,7 +8,8 @@ from allmydata.util import testutil
 class Basic(testutil.SignalMixin, unittest.TestCase):
     def test_loadable(self):
         q = introducer_and_vdrive.IntroducerAndVdrive()
-        d = q.startService()
+        d = fireEventually(None)
+        d.addCallback(lambda res: q.startService())
         d.addCallback(lambda res: q.stopService())
         d.addCallback(flushEventualQueue)
         return d
