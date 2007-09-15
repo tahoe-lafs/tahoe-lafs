7bb7d76d9df85bcfc15c260bf3060050de58aa3e
Brian Warner <warner@lothar.com>
2007-09-15 02:31:40 -0700

    build-deps-setup.py: use ez_setup here too, for platforms without setuptools

--- misc/dependencies/build-deps-setup.py
+++ misc/dependencies/build-deps-setup.py
@@ -1,5 +1,13 @@
 #! /usr/bin/python
 
+from ez_setup import use_setuptools
+import sys
+if 'cygwin' in sys.platform.lower():
+    min_version='0.6c6'
+else:
+    min_version='0.6a9'
+use_setuptools(min_version=min_version, download_base="file:misc/dependencies/")
+
 from setuptools import setup
 
 setup(name='tahoe-deps',
