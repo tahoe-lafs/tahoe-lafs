5cedf89a674f1f18d6dcf5fd6eede6f678f449f8
Brian Warner <warner@lothar.com>
2007-09-15 14:04:57 -0700

    build-deps-setup.py: provide dependency_links too

--- misc/dependencies/build-deps-setup.py
+++ misc/dependencies/build-deps-setup.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 
+# N.B.: this expects to run from the top of the source tree
+
+import os, sys
 from ez_setup import use_setuptools
-import sys
 if 'cygwin' in sys.platform.lower():
     min_version='0.6c6'
 else:
@@ -10,11 +12,18 @@ use_setuptools(min_version=min_version, download_base="file:misc/dependencies/")
 
 from setuptools import setup
 
+dependency_tarballs=[ os.path.join("misc", "dependencies", fn)
+                      for fn in os.listdir(os.path.join("misc", "dependencies"))
+                      if fn.endswith(".tar.gz") ]
+dependency_links=["http://allmydata.org/trac/tahoe/wiki/Dependencies"] + dependency_tarballs
+
 setup(name='tahoe-deps',
       version="1",
       install_requires=["zfec >= 1.0.3",
-                        "foolscap >= 0.1.6", "simplejson >= 1.4",
+                        "foolscap >= 0.1.6",
+                        "simplejson >= 1.4",
                         #"nevow", # we need nevow, but it doesn't seem to be
                                   # installable by easy_install
                         ],
+      dependency_links=dependency_links,
       )
