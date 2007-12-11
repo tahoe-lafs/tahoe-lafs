0618cf792d3dd43631aed0f1752b9db943842e1b
Brian Warner <warner@allmydata.com>
2007-12-10 20:06:59 -0700

    build-deps: require setuptools 0.6c4 or later, because older ones don't allow foolscap to use os.random at import time

--- misc/dependencies/build-deps-setup.py
+++ misc/dependencies/build-deps-setup.py
@@ -7,8 +7,13 @@ from ez_setup import use_setuptools
 if 'cygwin' in sys.platform.lower():
     min_version='0.6c6'
 else:
-    min_version='0.6a9'
-use_setuptools(min_version=min_version, download_base="file:misc/dependencies/")
+    # foolscap uses a module-level os.urandom() during import, which
+    # breaks inside older setuptools' sandboxing. 0.6c4 is the first
+    # version which fixed this problem.
+    min_version='0.6c4'
+use_setuptools(min_version=min_version,
+               download_base="file:misc/dependencies/",
+               download_delay=0)
 
 from setuptools import setup
 
--- setup.py
+++ setup.py
@@ -29,8 +29,13 @@ else:
     if 'cygwin' in sys.platform.lower():
         min_version='0.6c6'
     else:
-        min_version='0.6a9'
-    use_setuptools(min_version=min_version, download_base="file:misc/dependencies/", download_delay=0)
+        # foolscap uses a module-level os.urandom() during import, which
+        # breaks inside older setuptools' sandboxing. 0.6c4 is the first
+        # version which fixed this problem.
+        min_version='0.6c4'
+        use_setuptools(min_version=min_version, download_delay=0,
+                       download_base="file:misc/dependencies/",
+                       download_delay=0)
 
 from setuptools import Extension, find_packages, setup
 
