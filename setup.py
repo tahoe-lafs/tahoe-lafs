6809fa659eb22f1f51200654e6d41c2889832236
Zooko O'Whielacronx <zooko@zooko.com>
2007-12-20 16:13:10 -0700

    setup: fix the name of "misc/dependencies" for bootstrapping setuptools

--- setup.py
+++ setup.py
@@ -33,8 +33,9 @@ else:
         # breaks inside older setuptools' sandboxing. 0.6c4 is the first
         # version which fixed this problem.
         min_version='0.6c4'
+    downloadbase = os.path.join('misc', 'dependencies')+os.path.sep
     use_setuptools(min_version=min_version,
-                   download_base="file:%s" % os.path.join('misc', 'dependencies'),
+                   download_base=download_base,
                    download_delay=0)
 
 from setuptools import Extension, find_packages, setup
