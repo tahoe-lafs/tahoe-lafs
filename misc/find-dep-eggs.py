b41284748546e6a1cc3960aeab36c895f6280152
Zooko O'Whielacronx <zooko@zooko.com>
2008-01-01 01:53:31 -0700

    setup: whoops, fix the use of source-tree-root-dir eggs in our Makefile

--- misc/find-dep-eggs.py
+++ misc/find-dep-eggs.py
@@ -14,4 +14,10 @@ if os.path.exists(support_lib):
         if fn.endswith(".egg"):
             path.append(os.path.abspath(os.path.join(support_lib, fn)))
 
+# We also need to include .egg's in the CWD, because if there is an .egg there
+# then "make build-deps" will take that as satisfying its requirements.
+for fn in os.listdir("."):
+    if fn.endswith(".egg"):
+        path.append(os.path.abspath(os.path.join(os.getcwd(), fn)))
+
 print os.pathsep.join(path)
