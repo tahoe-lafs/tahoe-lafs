d9d16979960056d7c83870f89e16762ecd488bed
Brian Warner <warner@allmydata.com>
2008-01-07 17:32:45 -0700

    util.log: add levels like UNUSUAL

--- src/allmydata/util/log.py
+++ src/allmydata/util/log.py
@@ -2,6 +2,16 @@
 from foolscap.logging import log
 from twisted.python import failure
 
+NOISY = log.NOISY # 10
+OPERATIONAL = log.OPERATIONAL # 20
+UNUSUAL = log.UNUSUAL # 23
+INFREQUENT = log.INFREQUENT # 25
+CURIOUS = log.CURIOUS # 28
+WEIRD = log.WEIRD # 30
+SCARY = log.SCARY # 35
+BAD = log.BAD # 40
+
+
 msg = log.msg
 
 def err(f=None, **kwargs):
