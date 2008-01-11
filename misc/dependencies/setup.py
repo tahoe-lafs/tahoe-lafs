16e2b3307a9f2f8357cf2b2841f7c3af9a1de310
Brian Warner <warner@allmydata.com>
2008-01-10 20:15:02 -0700

    build-deps-setup.py: import twisted early, to make setuptools on dapper use the right version

--- misc/dependencies/setup.py
+++ misc/dependencies/setup.py
@@ -6,6 +6,21 @@ import sys, os
 
 miscdeps=os.path.join('misc', 'dependencies')
 
+# Dapper ships with older versions of Twisted (2.2.0) and Nevow (0.6.0), and
+# (unlike newer distributions) they are not installed with .egg meta-data
+# directories. As a result, they are invisible to setuptools. When the
+# 'build-deps' target thus builds Nevow, it will fail unless Twisted-2.4.0 or
+# newer is available, so Dapper users must install a newer Twisted before
+# running 'make build-deps'. In addition, through some not-yet-understood
+# quirk of setuptools, if that newer Twisted is in /usr/local/lib , somehow
+# the build still manages to pick up the old version from /usr/lib . It turns
+# out that importing twisted now, before use_setuptools() is called, causes
+# setuptools to stick with the correct (newer) Twisted. This causes an error
+# if Twisted is not installed before you run 'make build-deps', but having
+# Twisted at this point is a requirement anyways.
+
+import twisted
+
 try:
     from ez_setup import use_setuptools
 except ImportError:
