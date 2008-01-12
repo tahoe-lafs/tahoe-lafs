29277ed97bfa638432d25c0dfd39f67f9ace47df
robk-tahoe <robk-tahoe@allmydata.com>
2008-01-11 18:42:27 -0700

    add confwiz to py2exe build
    including setting up the windows xp look and feel stuff.

--- windows/setup.py
+++ windows/setup.py
@@ -3,11 +3,43 @@ import py2exe
 
 import glob
 
+lnf_manifest = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1"
+manifestVersion="1.0">
+<assemblyIdentity
+    version="0.64.1.0"
+    processorArchitecture="x86"
+    name="Controls"
+    type="win32"
+/>
+<description>%s</description>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+"""
+
+
 setup_args = {
     'name': 'Tahoe',
     'description': 'Allmydata Tahoe distributated storage',
     'author': 'Allmydata, Inc.',
     'windows': [
+        {
+            'script': 'confwiz.py',
+            'icon_resources': [(1, 'amdicon.ico')],
+            'other_resources': [(24,1,lnf_manifest%'Allmydata Tahoe Config Wizard')],
+        },
     ],
     'console': [
         'tahoe.py',
