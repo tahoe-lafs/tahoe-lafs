7e4ca29e33297e47ead3683ebca7bb9d4c009d45
Brian Warner <warner@allmydata.com>
2008-01-29 13:53:21 -0700

    _auto_deps.py: update comment

--- _auto_deps.py
+++ _auto_deps.py
@@ -23,11 +23,14 @@ def require_auto_deps():
             except pkg_resources.DistributionNotFound:
                 # there is no .egg-info present for this requirement, which
                 # either means that it isn't installed, or it is installed in
-                # a way that setuptools can't find it (but regular python
+                # a way that pkg_resources can't find it (but regular python
                 # might). The __import__ below will pass the second case,
                 # which is good enough for us. There are several
                 # distributions which provide our dependencies just fine, but
-                # they don't ship .egg-info files.
+                # they don't ship .egg-info files. Note that if there *is* an
+                # .egg-info file, but it indicates an older version, then
+                # we'll get a VersionConflict error instead of
+                # DistributionNotFound.
                 pass
     for requirement in install_requires:
         reqparts = requirement.split()
