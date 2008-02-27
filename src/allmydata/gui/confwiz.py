923c3e5fb50ea90b6602446fb35583094208c373
Peter Secor <secorp@allmydata.com>
2008-02-26 19:34:39 -0700

    installer and config - name changes

--- src/allmydata/gui/confwiz.py
+++ src/allmydata/gui/confwiz.py
@@ -190,7 +190,7 @@ class ConfWizApp(wx.App):
 
 class LoginFrame(wx.Frame):
     def __init__(self, app):
-        title = 'Allmydata Tahoe Config Wizard'
+        title = 'Allmydata Config Wizard'
         wx.Frame.__init__(self, None, -1, title)
         self.app = app
         self.SetIcon(amdicon.getIcon())
