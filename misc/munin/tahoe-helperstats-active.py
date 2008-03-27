6b416fc28c43cbcefebc0fc99a90cb32b25620c9
Brian Warner <warner@allmydata.com>
2008-03-27 16:50:30 -0700

    helper: add another munin plugin

---
+++ misc/munin/tahoe-helperstats-active.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+import os, sys
+import urllib
+import simplejson
+
+configinfo = """\
+graph_title Tahoe Helper Stats - Active Files
+graph_vlabel bytes
+graph_info This graph shows the number of files being actively processed by the helper
+fetched.label Active Files
+fetched.draw LINE2
+"""
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "config":
+        print configinfo.rstrip()
+        sys.exit(0)
+
+url = os.environ["url"]
+
+data = simplejson.loads(urllib.urlopen(url).read())
+print "fetched.value %d" % data["CHK_active_uploads"]
+
