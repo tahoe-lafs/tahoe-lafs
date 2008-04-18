9613997ddbed652224c4661c9059d2ec2e7311cf
Zooko O'Whielacronx <zooko@zooko.com>
2008-04-18 13:24:59 -0700

    setup: remove the try: except: around the import of pkg_resources -- we now require setuptools at run time and at build time

--- _auto_deps.py
+++ _auto_deps.py
@@ -14,26 +14,17 @@ if hasattr(sys, 'frozen'):
     install_requires=[]
 
 def require_auto_deps():
-    try:
-        import pkg_resources
-    except:
-        # Then we can't assert that the versions of these packages are the right
-        # versions, but we can still try to use them anyway...
-        pass
-    else:
-        for requirement in install_requires:
-            try:
-                pkg_resources.require(requirement)
-            except pkg_resources.DistributionNotFound:
-                # there is no .egg-info present for this requirement, which
-                # either means that it isn't installed, or it is installed in a
-                # way that pkg_resources can't find it (but regular python
-                # might).  There are several older Linux distributions which
-                # provide our dependencies just fine, but they don't ship
-                # .egg-info files. Note that if there *is* an .egg-info file,
-                # but it shows a too-old version, then we'll get a
-                # VersionConflict error instead of DistributionNotFound.
-                pass
-
-if __name__ == "__main__":
-    require_auto_deps()
+    import pkg_resources
+    for requirement in install_requires:
+        try:
+            pkg_resources.require(requirement)
+        except pkg_resources.DistributionNotFound:
+            # there is no .egg-info present for this requirement, which
+            # either means that it isn't installed, or it is installed in a
+            # way that pkg_resources can't find it (but regular python
+            # might).  There are several older Linux distributions which
+            # provide our dependencies just fine, but they don't ship
+            # .egg-info files. Note that if there *is* an .egg-info file,
+            # but it shows a too-old version, then we'll get a
+            # VersionConflict error instead of DistributionNotFound.
+            pass
