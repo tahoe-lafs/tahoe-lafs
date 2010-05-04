122e01e7104c8f3971e7c3e6b286649cf85b7b7a
Zooko O'Whielacronx <zooko@zooko.com>
2010-05-03 21:56:43 -0700

    setup: reorder and extend the show-tool-versions script, the better to glean information about our new buildslaves

--- misc/show-tool-versions.py
+++ misc/show-tool-versions.py
@@ -1,15 +1,65 @@
 #! /usr/bin/env python
 
-import sys
-import subprocess
+import os, subprocess, sys
+
+try:
+    import platform
+    out = platform.platform()
+    print
+    print "platform:", out.replace("\n", " ")
+except EnvironmentError, le:
+    sys.stderr.write("Got exception using 'platform': %s" % (le,))
+    pass
 
 print "python:", sys.version.replace("\n", " ") + ', maxunicode: ' + str(sys.maxunicode)
+
+try:
+    import pkg_resources
+    out = str(pkg_resources.require("setuptools"))
+    print
+    print "setuptools:", out.replace("\n", " ")
+except (ImportError, EnvironmentError), le:
+    sys.stderr.write("Got exception using 'pkg_resources' to get the version of setuptools: %s" % (le,))
+    pass
 
 try:
     out = subprocess.Popen(["buildbot", "--version"],
                            stdout=subprocess.PIPE).communicate()[0]
     print "buildbot:", out.replace("\n", " ")
-except EnvironmentError:
+except EnvironmentError, le:
+    sys.stderr.write("Got exception invoking 'buildbot': %s" % (le,))
+    pass
+
+try:
+    out = subprocess.Popen(["cl"],
+                           stdout=subprocess.PIPE).communicate()[0]
+    print "cl:", out.replace("\n", " ")
+except EnvironmentError, le:
+    sys.stderr.write("Got exception invoking 'cl': %s" % (le,))
+    pass
+
+try:
+    out = subprocess.Popen(["g++", "--version"],
+                           stdout=subprocess.PIPE).communicate()[0]
+    print "g++:", out.replace("\n", " ")
+except EnvironmentError, le:
+    sys.stderr.write("Got exception invoking 'g++': %s" % (le,))
+    pass
+
+try:
+    out = subprocess.Popen(["gcc", "--version"],
+                           stdout=subprocess.PIPE).communicate()[0]
+    print "gcc:", out.replace("\n", " ")
+except EnvironmentError, le:
+    sys.stderr.write("Got exception invoking 'gcc': %s" % (le,))
+    pass
+
+try:
+    out = subprocess.Popen(["as", "-version"], stdin=open(os.devnull),
+                           stdout=subprocess.PIPE).communicate()[0]
+    print "as:", out.replace("\n", " ")
+except EnvironmentError, le:
+    sys.stderr.write("Got exception invoking 'as': %s" % (le,))
     pass
 
 try:
@@ -20,21 +70,6 @@ try:
     print
     print "darcs:", out.replace("\n", " ")
     print full.rstrip()
-except EnvironmentError:
-    pass
-
-try:
-    import platform
-    out = platform.platform()
-    print
-    print "platform:", out.replace("\n", " ")
-except EnvironmentError:
-    pass
-
-try:
-    import pkg_resources
-    out = str(pkg_resources.require("setuptools"))
-    print
-    print "setuptools:", out.replace("\n", " ")
-except (ImportError, EnvironmentError):
+except EnvironmentError, le:
+    sys.stderr.write("Got exception invoking 'darcs': %s" % (le,))
     pass
