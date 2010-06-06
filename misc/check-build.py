bcb03583d783fc0386d57437145f6683de870075
Zooko O'Whielacronx <zooko@zooko.com>
2010-06-06 10:57:17 -0700

    setup: loosen the Desert Island test to allow it to check the network for new packages as long as it doesn't actually download any (You can look but don't touch.)

--- misc/check-build.py
+++ misc/check-build.py
@@ -12,10 +12,22 @@ print
 
 for line in open(build_out, "r"):
     if mode == "no-downloads":
-        # when setup_requires= uses misc/dependencies/setuptools-0.6c8.egg,
-        # it causes a "Downloading: misc/dependencies/.." line to be emitted,
-        # which doesn't count as a network download.
-        if line.startswith("Reading ") or line.startswith("Downloading http:"):
+        # when setup_requires= uses
+        # misc/dependencies/setuptools-0.6c8.egg, it causes a
+        # "Downloading: misc/dependencies/.." line to be emitted,
+        # which doesn't count as a network download.  Lines that start
+        # with "Reading" indicate that it is fetching web pages in
+        # order to check for newer versions of packages. As long as it
+        # doesn't actually download any packages then it still passes
+        # this test. That is: it *would* have succeeded if you were on
+        # a Desert Island, an airplane with no network, behind a
+        # corporate firewall that disallows such connections, or if
+        # you had turned off your network prior to running "python
+        # setup.py build". A stronger requirement would be that it
+        # doesn't even try to check for new packages on remote hosts
+        # if it has all the packages that it needs locally, but we
+        # currently don't enforce that stronger requirement.
+        if line.startswith("Downloading http:"):
             print line,
             good = False
 if good:
