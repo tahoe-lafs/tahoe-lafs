7cda268a780b918b3df5bd8545186308bd413f61
Zooko O'Whielacronx <zooko@zooko.com>
2010-11-17 19:06:23 -0800

    setup: show-tool-versions: include the version of valgrind

--- misc/build_helpers/show-tool-versions.py
+++ misc/build_helpers/show-tool-versions.py
@@ -158,6 +158,7 @@ print_stdout(['darcs', '--version'])
 print_stdout(['darcs', '--exact-version'], label='darcs-exact-version')
 print_stdout(['7za'])
 print_stdout(['flappclient', '--version'])
+print_stdout(['valgrind', '--version'])
 
 print_as_ver()
 
