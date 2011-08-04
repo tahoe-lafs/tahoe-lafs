b9eb0235ea38ce37b908399751709ce37c1c27d9
david-sarah <david-sarah@jacaranda.org>
2011-08-03 17:30:32 -0700

    test_runner.py: fix a bug in CreateNode.do_create introduced in changeset [5114] when the tahoe.cfg file has been written with CRLF line endings. refs #1385

--- src/allmydata/test/test_runner.py
+++ src/allmydata/test/test_runner.py
@@ -246,7 +246,7 @@ class CreateNode(unittest.TestCase):
             # 'create-node', and disabled for 'create-client'.
             tahoe_cfg = os.path.join(n1, "tahoe.cfg")
             self.failUnless(os.path.exists(tahoe_cfg))
-            content = fileutil.read(tahoe_cfg)
+            content = fileutil.read(tahoe_cfg).replace('\r\n', '\n')
             if kind == "client":
                 self.failUnless(re.search(r"\n\[storage\]\n#.*\nenabled = false\n", content), content)
             else:
