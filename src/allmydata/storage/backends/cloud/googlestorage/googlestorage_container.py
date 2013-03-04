1bcb17709e2eed312db226dea203897492501f2e
Itamar Turner-Trauring <itamar@futurefoundries.com>
2013-03-04 10:53:45 -0500

    Tests for googlestorage_container.AuthenticationClient.
    Author: Itamar Turner-Trauring <itamar@futurefoundries.com>

--- src/allmydata/storage/backends/cloud/googlestorage/googlestorage_container.py
+++ src/allmydata/storage/backends/cloud/googlestorage/googlestorage_container.py
@@ -6,7 +6,7 @@ http://code.google.com/p/google-api-python-client/downloads/list
 
 import httplib2
 
-from twisted.internet.defer import DeferredLock
+from twisted.internet.defer import DeferredLock, maybeDeferred
 from twisted.internet.threads import deferToThread
 
 from oauth2client.client import SignedJwtAssertionCredentials
@@ -34,29 +34,48 @@ class AuthenticationClient(object):
     more details.
     """
 
-    def __init__(self, account_name, private_key, private_key_password='notasecret'):
-        self.credentials = SignedJwtAssertionCredentials(
+    def __init__(self, account_name, private_key, private_key_password='notasecret',
+                 _credentialsClass=SignedJwtAssertionCredentials,
+                 _deferToThread=deferToThread):
+        # Google ships pkcs12 private keys encrypted with "notasecret" as the
+        # password. In order for automated running to work we'd need to
+        # include the password in the config file, so it adds no extra
+        # security even if someone chooses a different password. So it's seems
+        # simplest to hardcode it for now and it'll work with unmodified
+        # private keys issued by Google.
+        self._credentials = _credentialsClass(
             account_name, private_key,
             "https://www.googleapis.com/auth/devstorage.read_write",
             private_key_password = private_key_password,
             )
+        self._deferToThread = _deferToThread
         self._need_first_auth = True
         self._lock = DeferredLock()
+        # Get initial token:
+        self._refresh_if_necessary(force=True)
+
+    def _refresh_if_necessary(self, force=False):
+        """
+        Get a new authorization token, if necessary.
+        """
+        def run():
+            if force or self._credentials.access_token_expired:
+                # Generally using a task-specific thread pool is better than using
+                # the reactor one. However, this particular call will only run
+                # once an hour, so it's not likely to tie up all the threads.
+                return self._deferToThread(self._credentials.refresh, httplib2.Http())
+        return self._lock.run(run)
 
     def get_authorization_header(self):
         """
         Return a Deferred that fires with the value to use for the
         Authorization header in HTTP requests.
         """
-        def refreshIfNecessary():
-            if self._need_first_auth or self.credentials.access_token_expired:
-                self._need_first_auth = False
-                return deferToThread(self.credentials.refresh, httplib2.Http())
-        d = self._lock.run(refreshIfNecessary)
+        d = self._refresh_if_necessary()
 
         def refreshed(ignore):
             headers = {}
-            self.credentials.apply(headers)
+            self._credentials.apply(headers)
             return headers['Authorization']
         d.addCallback(refreshed)
         return d
