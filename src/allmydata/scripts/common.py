9774d38dc5e9e2d3c7fb2cfda7eb2ddff7cf159a
Leif Ryge <leif@synthesize.us>
2014-01-20 02:17:01 +0000

    bring back logdir runtime option
      turns out it was actually necessary if you want to run things

--- src/allmydata/scripts/common.py
+++ src/allmydata/scripts/common.py
@@ -41,7 +41,7 @@ class BasedirOptions(BaseOptions):
     optParameters = [
         ["basedir", "C", None, "Same as --node-directory (default %s)."
          % get_default_nodedir()],
-        ["incidents_dir", "I", None, "Set directory to save incident reports."],
+        ["logdir", "L", None, "Set directory to save twistd.log"],
     ]
 
     def parseArgs(self, basedir=None):
