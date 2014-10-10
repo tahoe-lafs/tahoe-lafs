4b1c65d1497dc2e0c2e379545be25d5257b441e8
zancas <zancas@leastauthority.com>
2014-10-10 10:29:16 -0600

    refactor unittests, fix style, add test
    (0) use CommonFixture as mixin to increase DRYness
    (1) self.failUnlessIn('size', metadata.keys()) --> self.failUnlessIn('size', metdata)
    (2) test_size_is_not_None --> test_size_is_0 AND test_size_is_1000

--- src/allmydata/test/test_JSONMetadata.py
+++ src/allmydata/test/test_JSONMetadata.py
@@ -5,62 +5,44 @@ from twisted.trial.unittest import TestCase
 from allmydata.web.common import get_filenode_metadata, SDMF_VERSION, MDMF_VERSION
 from allmydata.immutable.filenode import ImmutableFileNode
 
-class CommonFixture(TestCase):
+class CommonFixture(object):
     def setUp(self):
         self.mockfilenode = mock.Mock()
 
-
-class Test_GetFileNodeMetaData_Immutable(CommonFixture):
-    def setUp(self):
-        CommonFixture.setUp(self)
-        self.mockfilenode.is_mutable.return_value = False
-
-    def test_size_not_None(self):
+    def test_size_is_0(self):
         """If get_size doesn't return None the returned metadata must contain "size"."""
-        self.mockfilenode.get_size.return_value = 100
+        self.mockfilenode.get_size.return_value = 0
         metadata = get_filenode_metadata(self.mockfilenode)
-        self.failUnlessIn('size', metadata.keys())
+        self.failUnlessIn('size', metadata)
+
+    def test_size_is_1000(self):
+        """If get_size doesn't return None the returned metadata must contain "size"."""
+        self.mockfilenode.get_size.return_value = 1000
+        metadata = get_filenode_metadata(self.mockfilenode)
+        self.failUnlessIn('size', metadata)
 
     def test_size_is_None(self):
         """If get_size returns None the returned metadata must not contain "size"."""
         self.mockfilenode.get_size.return_value = None
         metadata = get_filenode_metadata(self.mockfilenode)
-        self.failIfIn('size', metadata.keys())
+        self.failIfIn('size', metadata)
 
 
-class Test_GetFileNodeMetaData_SDMF(CommonFixture):
+class Test_GetFileNodeMetaData_Immutable(CommonFixture, TestCase):
+    def setUp(self):
+        CommonFixture.setUp(self)
+        self.mockfilenode.is_mutable.return_value = False
+
+
+class Test_GetFileNodeMetaData_SDMF(CommonFixture, TestCase):
     def setUp(self):
         CommonFixture.setUp(self)
         self.mockfilenode.is_mutable.return_value = True
         self.mockfilenode.get_version.return_value = SDMF_VERSION
 
-    def test_size_not_None(self):
-        """If get_size doesn't return None the returned metadata must contain "size"."""
-        self.mockfilenode.get_size.return_value = 100
-        metadata = get_filenode_metadata(self.mockfilenode)
-        self.failUnlessIn('size', metadata.keys())
 
-    def test_size_is_None(self):
-        """If get_size returns None the returned metadata must not contain "size"."""
-        self.mockfilenode.get_size.return_value = None
-        metadata = get_filenode_metadata(self.mockfilenode)
-        self.failIfIn('size', metadata.keys())
-
-
-class Test_GetFileNodeMetaData_MDMF(CommonFixture):
+class Test_GetFileNodeMetaData_MDMF(CommonFixture, TestCase):
     def setUp(self):
         CommonFixture.setUp(self)
         self.mockfilenode.is_mutable.return_value = True
         self.mockfilenode.get_version.return_value = MDMF_VERSION
-
-    def test_size_not_None(self):
-        """If get_size doesn't return None the returned metadata must contain "size"."""
-        self.mockfilenode.get_size.return_value = 100
-        metadata = get_filenode_metadata(self.mockfilenode)
-        self.failUnlessIn('size', metadata.keys())
-
-    def test_size_is_None(self):
-        """If get_size returns None the returned metadata must not contain "size"."""
-        self.mockfilenode.get_size.return_value = None
-        metadata = get_filenode_metadata(self.mockfilenode)
-        self.failIfIn('size', metadata.keys())
