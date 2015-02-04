86726729b7acb3f46a25914073a2083533c2b6ee
Daira Hopwood <daira@jacaranda.org>
2015-02-04 00:10:21 +0000

    Quote the default node-directory correctly in help output. refs #2235
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/allmydata/scripts/common.py
+++ src/allmydata/scripts/common.py
@@ -3,7 +3,8 @@ import os, sys, urllib
 import codecs
 from twisted.python import usage
 from allmydata.util.assertutil import precondition
-from allmydata.util.encodingutil import unicode_to_url, quote_output, argv_to_abspath
+from allmydata.util.encodingutil import unicode_to_url, quote_output, \
+    quote_local_unicode_path, argv_to_abspath
 from allmydata.util.fileutil import abspath_expanduser_unicode
 
 
@@ -40,7 +41,7 @@ class BasedirOptions(BaseOptions):
 
     optParameters = [
         ["basedir", "C", None, "Specify which Tahoe base directory should be used. [default: %s]"
-         % get_default_nodedir()],
+         % quote_local_unicode_path(_default_nodedir)],
     ]
 
     def parseArgs(self, basedir=None):
