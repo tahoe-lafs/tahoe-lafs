7d6b9f823f3490a5385b9582d29a7eebc65a1120
David Stainton <dstainton415@gmail.com>
2015-06-18 10:50:47 -0700

    Add boiler plate for magic-folder subcommand
    here we also:
    - add the synopsis for the create command
    - add the argument parser for the create command

---
+++ src/allmydata/scripts/magic_folder_cli.py
@@ -0,0 +1,71 @@
+
+from twisted.python import usage, failure
+from allmydata.scripts.common import BaseOptions
+from .common import BaseOptions, BasedirOptions
+
+class CreateOptions(BasedirOptions):
+    nickname = None
+    localdir = None
+    def parseArgs(self, alias, nickname=None, localdir=None):
+        BasedirOptions.parseArgs(self)
+        self.alias = alias
+        self.nickname = nickname
+        self.localdir = localdir
+        if self.nickname and not self.localdir:
+            raise usage.UsageError("must provide both")
+    synopsis = "MAGIC_ALIAS: [NICKNAME LOCALDIR]"
+
+def create(options):
+    pass
+
+class InviteOptions(BasedirOptions):
+    pass
+
+def invite(options):
+    pass
+
+class JoinOptions(BasedirOptions):
+    pass
+
+def join(options):
+    pass
+
+class MagicFolderCommand(BaseOptions):
+    subCommands = [
+        ["create", None, CreateOptions, "Create a Magic-Folder."],
+        ["invite", None, InviteOptions, "Invite someone to a Magic-Folder."],
+        ["join", None, JoinOptions, "Join a Magic-Folder."],
+    ]
+    def postOptions(self):
+        if not hasattr(self, 'subOptions'):
+            raise usage.UsageError("must specify a subcommand")
+    def getSynopsis(self):
+        return "Usage: tahoe [global-options] magic SUBCOMMAND"
+    def getUsage(self, width=None):
+        t = BaseOptions.getUsage(self, width)
+        t += """\
+Please run e.g. 'tahoe magic-folder create --help' for more details on each
+subcommand.
+"""
+        return t
+
+subDispatch = {
+    "create": create,
+    "invite": invite,
+    "join": join,
+}
+
+def do_magic_folder(options):
+    so = options.subOptions
+    so.stdout = options.stdout
+    so.stderr = options.stderr
+    f = subDispatch[options.subCommand]
+    return f(so)
+
+subCommands = [
+    ["magic-folder", None, MagicFolderCommand, "magic-folder subcommands: use 'tahoe magic-folder' for a list."],
+]
+
+dispatch = {
+    "magic-folder": do_magic_folder,
+}
