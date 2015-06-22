9165df9f298c708edea7f65bce8ed188264c661c
David Stainton <dstainton415@gmail.com>
2015-06-22 15:09:57 -0700

    Fix options passed to mkdir from magic invite
    - here we also clean up the magic folder cli unit tests

--- src/allmydata/test/test_cli_magic_folder.py
+++ src/allmydata/test/test_cli_magic_folder.py
@@ -14,27 +14,22 @@ from .test_cli import CLITestMixin
 class CreateMagicFolder(GridTestMixin, CLITestMixin, unittest.TestCase):
 
     def _create_magic_folder(self):
-        d = self.do_cli("magic-folder", "create", "my_magic_folder")
+        d = self.do_cli("magic-folder", "create", "magic")
         def _done((rc,stdout,stderr)):
-            self.failUnless("Alias 'my_magic_folder' created" in stdout)
+            self.failUnless("Alias 'magic' created" in stdout)
             self.failIf(stderr)
             aliases = get_aliases(self.get_clientdir())
-            self.failUnless("my_magic_folder" in aliases)
-            self.failUnless(aliases["my_magic_folder"].startswith("URI:DIR2:"))
+            self.failUnless("magic" in aliases)
+            self.failUnless(aliases["magic"].startswith("URI:DIR2:"))
         d.addCallback(_done)
         return d
 
-    def test_create(self):
-        self.basedir = "cli/MagicFolder/create"
-        self.set_up_grid()
-        return self._create_magic_folder()
-
     def _invite(self, ignore):
-        d = self.do_cli("magic-folder", "invite", "magicFolder1", "Nicki")
+        d = self.do_cli("magic-folder", "invite", "magic", u"Alice:")
         return d
 
-    def test_invite(self):
-        self.basedir = "cli/MagicFolder/invite"
+    def test_create_invite_join(self):
+        self.basedir = "cli/MagicFolder/create-invite-join"
         self.set_up_grid()
         d = self._create_magic_folder()
         d.addCallback(self._invite)
