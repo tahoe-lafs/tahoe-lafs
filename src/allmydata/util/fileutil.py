bfda47fef4e431699d72d8323fdf6bdbbc74445e
Daira Hopwood <daira@jacaranda.org>
2015-08-31 17:52:55 +0100

    replace_file should not fail if replaced_path does not exist.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/allmydata/util/fileutil.py
+++ src/allmydata/util/fileutil.py
@@ -619,7 +619,11 @@ else:
         if not os.path.exists(replacement_path):
             raise ConflictError("Replacement file not found: %r" % (replacement_path,))
         try:
-            os.rename(replaced_path, backup_path)
+            try:
+                os.rename(replaced_path, backup_path)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
             rename_no_overwrite(replacement_path, replaced_path)
         except EnvironmentError:
             reraise(ConflictError)
