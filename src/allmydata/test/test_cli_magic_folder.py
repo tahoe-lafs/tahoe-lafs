28620433c011a6eaa3739beb5278c896cfb2b423
Daira Hopwood <daira@jacaranda.org>
2015-09-03 20:20:13 +0100

    Remove self.client and stats_provider attributes which could get out-of-sync with self.get_client().
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/allmydata/test/test_cli_magic_folder.py
+++ src/allmydata/test/test_cli_magic_folder.py
@@ -83,9 +83,10 @@ class MagicFolderCLITestMixin(CLITestMixin, GridTestMixin):
             return (rc,stdout,stderr)
         d.addCallback(_done)
         def get_alice_caps(x):
+            client = self.get_client()
             self.collective_dircap, self.upload_dircap = self.get_caps_from_files(0)
-            self.collective_dirnode = self.client.create_node_from_uri(self.collective_dircap)
-            self.upload_dirnode     = self.client.create_node_from_uri(self.upload_dircap)
+            self.collective_dirnode = client.create_node_from_uri(self.collective_dircap)
+            self.upload_dirnode     = client.create_node_from_uri(self.upload_dircap)
         d.addCallback(get_alice_caps)
         d.addCallback(lambda x: self.check_joined_config(0, self.upload_dircap))
         d.addCallback(lambda x: self.check_config(0, local_dir))
