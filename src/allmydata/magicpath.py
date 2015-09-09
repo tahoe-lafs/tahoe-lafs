ee78679357ab3d40916b8c39a2ae900bac76b579
David Stainton <dstainton415@gmail.com>
2015-09-09 15:17:23 +0200

    Add should_ignore_file and tests
    This helper function will be used by magic-folder to
    determine when to drop a file from the upload queue...
    it ignores hidden files and certain suffixed files.

--- src/allmydata/magicpath.py
+++ src/allmydata/magicpath.py
@@ -1,9 +1,30 @@
 
 import re
-
+import os.path
 
 def path2magic(path):
     return re.sub(ur'[/@]',  lambda m: {u'/': u'@_', u'@': u'@@'}[m.group(0)], path)
 
 def magic2path(path):
     return re.sub(ur'@[_@]', lambda m: {u'@_': u'/', u'@@': u'@'}[m.group(0)], path)
+
+
+IGNORE_SUFFIXES = ['.backup', '.tmp', '.conflicted']
+IGNORE_PREFIXES = ['.']
+
+def should_ignore_file(path_u):
+    for suffix in IGNORE_SUFFIXES:
+        if path_u.endswith(suffix):
+            return True
+    while True:
+        head, tail = os.path.split(path_u)
+        if tail != "":
+            for prefix in IGNORE_PREFIXES:
+                if tail.startswith(prefix):
+                    return True
+                else:
+                    path_u = head
+        else:
+            if head == "":
+                return False
+    return False
