4ab0f603508eec9e70559963aa8eb3fa4882bd55
Daira Hopwood <daira@jacaranda.org>
2015-10-19 18:24:33 +0100

    Fix check for initial '-' in argv_to_abspath.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/allmydata/util/encodingutil.py
+++ src/allmydata/util/encodingutil.py
@@ -95,15 +95,17 @@ def argv_to_unicode(s):
     except UnicodeDecodeError:
         raise usage.UsageError("Argument %s cannot be decoded as %s." %
                                (quote_output(s), io_encoding))
-    if local_dir.startswith('-'):
-        raise usage.UsageError("Argument %s cannot start with a -." % (quote_output(s),))
 
 def argv_to_abspath(s):
     """
     Convenience function to decode an argv element to an absolute path, with ~ expanded.
     If this fails, raise a UsageError.
     """
-    return abspath_expanduser_unicode(argv_to_unicode(s))
+    decoded = argv_to_unicode(s)
+    if decoded.startswith(u'-'):
+        raise usage.UsageError("Path argument %s cannot start with '-'.\nUse %s if you intended to refer to a file."
+                               % (quote_output(s), quote_output(os.path.join('.', s))))
+    return abspath_expanduser_unicode(decoded)
 
 def unicode_to_argv(s, mangle=False):
     """
