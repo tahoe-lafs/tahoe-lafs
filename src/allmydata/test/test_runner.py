a999654d83ecc7a19df8cc73021bd18b60ca1ca7
David Stainton <dstainton415@gmail.com>
2015-12-08 03:16:33 +0200

    remove magic_folder from config in test

--- src/allmydata/test/test_runner.py
+++ src/allmydata/test/test_runner.py
@@ -266,8 +266,6 @@ class CreateNode(unittest.TestCase):
                 self.failUnless(re.search(r"\n\[storage\]\n#.*\nenabled = true\n", content), content)
                 self.failUnless("\nreserved_space = 1G\n" in content)
 
-            self.failUnless(re.search(r"\n\[magic_folder\]\n#.*\n#enabled = false\n", content), content)
-
         # creating the node a second time should be rejected
         rc, out, err = self.run_tahoe(argv)
         self.failIfEqual(rc, 0, str((out, err, rc)))
