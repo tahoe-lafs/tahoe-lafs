f8ce8f15eafa9e45f5421ed0f1f4997191c34938
David Stainton <dstainton415@gmail.com>
2015-12-17 13:10:09 +0200

    Fix typo

--- src/allmydata/test/test_configutil.py
+++ src/allmydata/test/test_configutil.py
@@ -17,7 +17,7 @@ class ConfigUtilTests(CLITestMixin, GridTestMixin, unittest.TestCase):
         # test that at least one option was read correctly
         config = configutil.get_config(tahoe_cfg)
         self.failUnlessEqual(config.get("node", "nickname"), "client-0")
-        
+
         # test that set_config can mutate an existing option
         configutil.set_config(config, "node", "nickname", "Alice!")
         configutil.write_config(tahoe_cfg, config)
@@ -26,7 +26,7 @@ class ConfigUtilTests(CLITestMixin, GridTestMixin, unittest.TestCase):
         self.failUnlessEqual(config.get("node", "nickname"), "Alice!")
 
         # test that set_config can set a new option
-        descriptor = "twas brillag and slithy toves did gear and gimble"
+        descriptor = "Twas brillig, and the slithy toves Did gyre and gimble in the wabe"
         configutil.set_config(config, "node", "descriptor", descriptor)
         configutil.write_config(tahoe_cfg, config)
 
