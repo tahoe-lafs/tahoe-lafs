bc64a6b2f802bff9b593b19292250e8a92cec9fe
Leif Ryge <leif@synthesize.us>
2016-01-04 19:58:55 +0000

    wui: improved columns in welcome page server list
    As discussed at https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1973 and in
    previous pull request #129.
    
     - replace lengthy timestamps with human-readable deltas (eg 1h 2m 3s)
     - replace "announced" column with "Last RX" column
     - remove service column (it always said the same thing, "storage")
     - fix colspan on 'You are not presently connected' message
    
    Previous versions, some with github comments: https://github.com/leif/tahoe-lafs/commit/3fe9053134b2429904f673df561e602a50f83c7e , https://github.com/leif/tahoe-lafs/commit/486dbfc7bd3c0bbba42a6df8e4564601120aec0e , and https://github.com/tahoe-lafs/tahoe-lafs/commit/c89ea625803be36a18bce1af4eef95dcd78bba2b, https://github.com/tahoe-lafs/tahoe-lafs/commit/9fabb924867e164e1c6d4d805761db6c39652cf7, https://github.com/tahoe-lafs/tahoe-lafs/commit/bbd8b42a25f8617c43b8293f3b654b3d060e27b9
    
    Unlike previous attempts, the tests on this one should pass in any timezone.
    (But like current master, will fail with Nevow >=0.12...)
    
    Thanks to an anonymous contributor who wrote some of the tests.

--- src/allmydata/test/test_util.py
+++ src/allmydata/test/test_util.py
@@ -1180,6 +1180,40 @@ class TimeFormat(unittest.TestCase, TimezoneMixin):
         self.failUnlessEqual(
             time_format.format_delta(time_1decimal, time_1d21h46m49s_delta), '1d 21h 46m 48s')
 
+    def test_format_delta(self):
+        time_1 = 1389812723
+        time_5s_delta = 1389812728
+        time_28m7s_delta = 1389814410
+        time_1h_delta = 1389816323
+        time_1d21h46m49s_delta = 1389977532
+
+        self.failUnlessEqual(
+            time_format.format_delta(time_1, time_1), '0s')
+
+        self.failUnlessEqual(
+            time_format.format_delta(time_1, time_5s_delta), '5s')
+        self.failUnlessEqual(
+            time_format.format_delta(time_1, time_28m7s_delta), '28m 7s')
+        self.failUnlessEqual(
+            time_format.format_delta(time_1, time_1h_delta), '1h 0m 0s')
+        self.failUnlessEqual(
+            time_format.format_delta(time_1, time_1d21h46m49s_delta), '1d 21h 46m 49s')
+
+        self.failUnlessEqual(
+            time_format.format_delta(time_1d21h46m49s_delta, time_1), '-')
+
+        # time_1 with a decimal fraction will make the delta 1s less
+        time_1decimal = 1389812723.383963
+
+        self.failUnlessEqual(
+            time_format.format_delta(time_1decimal, time_5s_delta), '4s')
+        self.failUnlessEqual(
+            time_format.format_delta(time_1decimal, time_28m7s_delta), '28m 6s')
+        self.failUnlessEqual(
+            time_format.format_delta(time_1decimal, time_1h_delta), '59m 59s')
+        self.failUnlessEqual(
+            time_format.format_delta(time_1decimal, time_1d21h46m49s_delta), '1d 21h 46m 48s')
+
 class CacheDir(unittest.TestCase):
     def test_basic(self):
         basedir = "test_util/CacheDir/test_basic"
