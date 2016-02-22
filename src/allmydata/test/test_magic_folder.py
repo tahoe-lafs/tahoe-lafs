63af9f801085a39ccd6b33d761d22f0bfe39b852
Daira Hopwood <daira@jacaranda.org>
2016-02-22 16:04:19 +0000

    WIP: add close method for MagicFolderDB and make MagicFolderDbTests use it.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/allmydata/test/test_magic_folder.py
+++ src/allmydata/test/test_magic_folder.py
@@ -42,6 +42,8 @@ class MagicFolderDbTests(unittest.TestCase):
         self.failUnlessEqual(self.db.VERSION, 1)
 
     def tearDown(self):
+        if hasattr(self, 'db'):
+            self.db.close()
         shutil.rmtree(self.temp)
         return super(MagicFolderDbTests, self).tearDown()
 
