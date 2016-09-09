442468f599085c493e9b958eeab40530715fa2a6
Brian Warner <warner@lothar.com>
2016-09-09 10:40:39 -0700

    do_cli(): split out run_cli()
    The main part of CLITestMixin.do_cli() was split into a standalone
    function named run_cli(), leaving do_cli() as a method which includes a
    nodedir in the arguments (for use by GridTestMixin tests which do a lot
    of CLI operations against one of their client nodes, for which adding
    the extra --nodedir argument would be ugly).

--- src/allmydata/test/cli/common.py
+++ src/allmydata/test/cli/common.py
@@ -14,22 +14,24 @@ def parse_options(basedir, command, args):
 
 class CLITestMixin(ReallyEqualMixin):
     def do_cli(self, verb, *args, **kwargs):
-        precondition(not [True for arg in args if not isinstance(arg, str)],
-                     "arguments to do_cli must be strs -- convert using unicode_to_argv", args=args)
-
-        # client_num is used to execute client CLI commands on a specific client.
+        # client_num is used to execute client CLI commands on a specific
+        # client.
         client_num = kwargs.get("client_num", 0)
+        client_dir = unicode_to_argv(self.get_clientdir(i=client_num))
+        nodeargs = [ "--node-directory", client_dir ]
+        return run_cli(verb, nodeargs=nodeargs, *args, **kwargs)
 
-        nodeargs = [
-            "--node-directory", unicode_to_argv(self.get_clientdir(i=client_num)),
-            ]
-        argv = nodeargs + [verb] + list(args)
-        stdin = kwargs.get("stdin", "")
-        stdout, stderr = StringIO(), StringIO()
-        d = threads.deferToThread(runner.runner, argv, run_by_human=False,
-                                  stdin=StringIO(stdin),
-                                  stdout=stdout, stderr=stderr)
-        def _done(rc):
-            return rc, stdout.getvalue(), stderr.getvalue()
-        d.addCallback(_done)
-        return d
+def run_cli(verb, *args, **kwargs):
+    precondition(not [True for arg in args if not isinstance(arg, str)],
+                 "arguments to do_cli must be strs -- convert using unicode_to_argv", args=args)
+    nodeargs = kwargs.get("nodeargs", [])
+    argv = nodeargs + [verb] + list(args)
+    stdin = kwargs.get("stdin", "")
+    stdout, stderr = StringIO(), StringIO()
+    d = threads.deferToThread(runner.runner, argv, run_by_human=False,
+                              stdin=StringIO(stdin),
+                              stdout=stdout, stderr=stderr)
+    def _done(rc):
+        return rc, stdout.getvalue(), stderr.getvalue()
+    d.addCallback(_done)
+    return d
