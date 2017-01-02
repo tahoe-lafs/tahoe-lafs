a5c135a06e4918457026046a8a476c8edacc2bde
Vladimir Rusinov <vrusinov@google.com>
2017-01-02 19:06:23 +0000

    Add test for api-version in inner 'stats' field of the deep mainfest operation.

--- src/allmydata/test/web/test_web.py
+++ src/allmydata/test/web/test_web.py
@@ -2051,6 +2051,7 @@ class Web(WebMixin, WebErrorMixin, testutil.StallMixin, testutil.ReallyEqualMixi
             self.failUnlessIn("verifycaps", res)
             self.failUnlessIn("stats", res)
             self.failUnlessIn("api-version", res)
+            self.failUnlessIn("api-version", res["stats"])
         d.addCallback(_got_json)
         return d
 
