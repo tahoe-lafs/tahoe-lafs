b0e291b431c91d7207dd9555c923cfc3a40b81db
meejah <meejah@meejah.ca>
2017-04-26 15:27:06 -0600

    remove comment to kick builders

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ def read_version_py(infname):
 if 2 != sys.version_info.major:
     raise RuntimeError("Python version 2 is required")
 
-# source all the other data
 VERSION_PY_FILENAME = 'src/allmydata/_version.py'
 version = read_version_py(VERSION_PY_FILENAME)
 
