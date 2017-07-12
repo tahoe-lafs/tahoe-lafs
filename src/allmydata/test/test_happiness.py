11c39325fb827656e1538b673e0f8200b55b745f
Brian Warner <warner@lothar.com>
2017-07-12 16:27:13 -0700

    test_happiness: suppress the hypothesis 'too_slow' check
    I was seeing buildbot failures where the host machine was too busy, and
    hypothesis was spooked into thinking that data generation was too slow.

--- src/allmydata/test/test_happiness.py
+++ src/allmydata/test/test_happiness.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from twisted.trial import unittest
-from hypothesis import given
+from hypothesis import given, settings, HealthCheck
 from hypothesis.strategies import text, sets
 from allmydata.immutable import happiness_upload
 
@@ -225,6 +225,7 @@ class PlacementTests(unittest.TestCase):
         sets(elements=text(min_size=1), min_size=4, max_size=4),
         sets(elements=text(min_size=1), min_size=4),
     )
+    @settings(suppress_health_check=[HealthCheck.too_slow])
     def test_hypothesis_unhappy(self, peers, shares):
         """
         similar to test_unhappy we test that the resulting happiness is
@@ -244,6 +245,7 @@ class PlacementTests(unittest.TestCase):
         # can we make a readonly_peers that's a subset of ^
         sets(elements=text(min_size=1), min_size=1, max_size=20),
     )
+    @settings(suppress_health_check=[HealthCheck.too_slow])
     def test_more_hypothesis(self, peers, shares):
         """
         similar to test_unhappy we test that the resulting happiness is
