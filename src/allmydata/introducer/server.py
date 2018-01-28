21f299ae6c59f1f869cff88cea28e1df082c992a
meejah <meejah@meejah.ca>
2018-01-28 01:27:25 -0700

    more refactor + fix some node tests

--- src/allmydata/introducer/server.py
+++ src/allmydata/introducer/server.py
@@ -47,14 +47,20 @@ def create_introducer(basedir=u"."):
         _valid_config_sections=_valid_config_sections,
     )
 
-    default_connection_handlers, foolscap_connection_handlers = create_connection_handlers(reactor, basedir, config)
+    # XXX fix up imports etc (also: reactor)
+    from twisted.internet import reactor
+    from allmydata.node import create_i2p_provider, create_tor_provider
+    i2p_provider = create_i2p_provider(reactor, basedir, config)
+    tor_provider = create_tor_provider(reactor, basedir, config)
+
+    default_connection_handlers, foolscap_connection_handlers = create_connection_handlers(reactor, basedir, config, i2p_provider, tor_provider)
     tub_options = create_tub_options(config)
 
     i2p_provider = None
     tor_provider = None
     main_tub, is_listening = create_main_tub(
         basedir, config, tub_options, default_connection_handlers,
-        foolscap_connection_handlers,
+        foolscap_connection_handlers, i2p_provider, tor_provider,
     )
     control_tub = create_control_tub()
 
