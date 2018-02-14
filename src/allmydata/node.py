f4155fa626f8cb3c700d880a70a6deb1140a8313
meejah <meejah@meejah.ca>
2018-02-13 18:57:49 -0700

    get_app_version -> global function

--- src/allmydata/node.py
+++ src/allmydata/node.py
@@ -159,6 +159,11 @@ def config_from_string(config_str, portnumfile, basedir):
     return _Config(parser, portnumfile, basedir, '<in-memory>')
 
 
+def get_app_versions():
+    # TODO: merge this with allmydata.get_package_versions
+    return dict(app_versions.versions)
+
+
 def _error_about_old_config_files(basedir, generated_files):
     """
     If any old configuration files are detected, raise
@@ -213,10 +218,6 @@ class _Config(object):
         except EnvironmentError:
             if os.path.exists(self.config_fname):
                 raise
-
-    def get_app_versions(self):
-        # TODO: merge this with allmydata.get_package_versions
-        return dict(app_versions.versions)
 
     def write_config_file(self, name, value, mode="w"):
         """
