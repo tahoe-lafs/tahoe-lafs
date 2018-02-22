8bf714db30ca7fed6dc9854d286195ba75e8b46e
meejah <meejah@meejah.ca>
2018-02-22 15:16:01 -0700

    _Config does this for us

--- src/allmydata/node.py
+++ src/allmydata/node.py
@@ -375,8 +375,6 @@ class Node(service.MultiService):
         is the current directory by default.
         """
         service.MultiService.__init__(self)
-        # XXX don't write files in ctor!
-        fileutil.make_dirs(os.path.join(config._basedir, "private"), 0700)
         with open(os.path.join(config._basedir, "private", "README"), "w") as f:
             f.write(PRIV_README)
 
