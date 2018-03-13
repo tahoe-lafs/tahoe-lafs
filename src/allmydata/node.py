629646d22f4d6c01e4b1c1fcd028e4796dd74036
meejah <meejah@meejah.ca>
2018-03-13 17:11:30 -0600

    reorder args to match other method

--- src/allmydata/node.py
+++ src/allmydata/node.py
@@ -196,11 +196,11 @@ def read_config(basedir, portnumfile, generated_files=[], _valid_config_sections
     return _Config(parser, portnumfile, basedir, config_fname)
 
 
-# XXX consider re-ordering args to match read_config()
-def config_from_string(config_str, portnumfile, basedir):
+def config_from_string(basedir, portnumfile, config_str):
     """
     load configuration from in-memory string
     """
+    # load configuration from in-memory string
     parser = ConfigParser.SafeConfigParser()
     parser.readfp(BytesIO(config_str))
     return _Config(parser, portnumfile, basedir, '<in-memory>')
