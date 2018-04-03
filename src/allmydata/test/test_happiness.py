6bca7495927bf5ac9498c1b4fcf45090bff32a39
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-04-03 13:59:35 -0400

    this is still needed

--- src/allmydata/test/test_happiness.py
+++ src/allmydata/test/test_happiness.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from twisted.trial import unittest
+from hypothesis import given
 from hypothesis.strategies import text, sets
 from allmydata.immutable import happiness_upload
 
