16e4370630fc6b4df56d52fe6020d6bc319f4a8c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-07-05 14:19:23 -0400

    Generate junitxml and give it to CircleCI

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -13,4 +13,22 @@ shift || :
 # Also run with /tmp as a workdir because the non-root user won't be able to
 # create the tox working filesystem state in the source checkout because it is
 # owned by root.
-sudo --set-home -u nobody /tmp/tests/bin/tox -c /tmp/project/tox.ini --workdir /tmp -e "${TAHOE_LAFS_TOX_ENVIRONMENT}" ${TAHOE_LAFS_TOX_ARGS}
+sudo --set-home -u nobody /tmp/tests/bin/tox \
+     -c /tmp/project/tox.ini \
+     --result-json /tmp/tox-result.json \
+     --workdir /tmp \
+     -e "${TAHOE_LAFS_TOX_ENVIRONMENT}" \
+     --reporter=subunit ${TAHOE_LAFS_TOX_ARGS}
+
+# Extract the test process output which should be subunit1-format.
+cat /tmp/tox-result.json | /tmp/tests/bin/python -c '
+from json import load
+from sys import stdin
+result = load(sys.stdin)
+messy_output = result["testenvs"]["py27"]["test"][-1]["output"]
+sys.stdout.write(messy_output.split("\n", 3)[3].strip())
+' > /tmp/test-result.subunit1
+
+# Convert the subunit1 data to junitxml which CircleCI can ingest.
+mkdir -p /tmp/junit
+subunit-1to2 < /tmp/test-result.subunit1 | subunit2junitxml > /tmp/junit/results.junitxml
