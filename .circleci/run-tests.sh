61f88c53594db5b101c4170e9eef7d6d6afa40c1
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-07-06 11:32:19 -0400

    nothing else will create it, I think

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -9,6 +9,9 @@ shift
 TAHOE_LAFS_TOX_ARGS=$1
 shift || :
 
+# Make sure we can actually write things to this directory.
+mkdir -p "${ARTIFACTS}"
+
 # Run the test suite as a non-root user.  This is the expected usage some
 # small areas of the test suite assume non-root privileges (such as unreadable
 # files being unreadable).
