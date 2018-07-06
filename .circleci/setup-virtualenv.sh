79bea48b580e95e553379e49e72fb21f5eb59589
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-07-06 10:54:31 -0400

    Get setup and build to agree on tox workdir

--- .circleci/setup-virtualenv.sh
+++ .circleci/setup-virtualenv.sh
@@ -21,4 +21,9 @@ REPORTING_DEPS="python-subunit junitxml"
 sudo --set-home -u nobody /tmp/tests/bin/pip install ${TEST_DEPS} ${REPORTING_DEPS}
 
 # Get everything else installed in it, too.
-sudo --set-home -u nobody /tmp/tests/bin/tox -c /tmp/project/tox.ini --workdir /tmp --notest -e "${TAHOE_LAFS_TOX_ENVIRONMENT}" ${TAHOE_LAFS_TOX_ARGS}
+sudo --set-home -u nobody /tmp/tests/bin/tox \
+     -c /tmp/project/tox.ini \
+     --workdir /tmp/tahoe-lafs.tox \
+     --notest \
+     -e "${TAHOE_LAFS_TOX_ENVIRONMENT}" \
+     ${TAHOE_LAFS_TOX_ARGS}
