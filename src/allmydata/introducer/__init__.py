7de94f623cd200e66f32a6a3ac0892f5113f4535
meejah <meejah@meejah.ca>
2018-09-06 11:56:20 -0600

    clarify Introducer imports

--- src/allmydata/introducer/__init__.py
+++ src/allmydata/introducer/__init__.py
@@ -1,12 +1,13 @@
 
-# This is for compatibilty with old .tac files, which reference
-# allmydata.introducer.IntroducerNode
-
 from allmydata.introducer.server import create_introducer
-# apparently need to support "old .tac files" that may have this name burned in
-# don't use this in new code
+
+# apparently need to support "old .tac files" that may have
+# "allmydata.introducer.IntroducerNode" burned in -- don't use this in
+# new code
 from allmydata.introducer.server import _IntroducerNode as IntroducerNode
 
 # hush pyflakes
-_unused = [create_introducer, IntroducerNode]
-del _unused
+__all__ = (
+    "create_introducer",
+    "IntroducerNode",
+)
