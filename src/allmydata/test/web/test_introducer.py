f5e287d3e99f35830c6f51365e6e7e90191fa443
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-03-28 15:10:43 -0400

    Use the port assigner in test_introducer

--- src/allmydata/test/web/test_introducer.py
+++ src/allmydata/test/web/test_introducer.py
@@ -1,15 +1,24 @@
 from os.path import join
 from twisted.trial import unittest
+from twisted.internet import reactor
 from foolscap.api import fireEventually, flushEventualQueue
 from twisted.internet import defer
 from allmydata.introducer import create_introducer
 from allmydata import node
-from .common import FAVICON_MARKUP
+from .common import (
+    FAVICON_MARKUP,
+)
+from ..common import (
+    SameProcessStreamEndpointAssigner,
+)
 from ..common_web import do_http
 
 class IntroducerWeb(unittest.TestCase):
     def setUp(self):
         self.node = None
+        self.port_assigner = SameProcessStreamEndpointAssigner()
+        self.port_assigner.setUp()
+        self.addCleanup(self.port_assigner.tearDown)
 
     def tearDown(self):
         d = defer.succeed(None)
@@ -22,11 +31,12 @@ class IntroducerWeb(unittest.TestCase):
     def test_welcome(self):
         basedir = self.mktemp()
         node.create_node_dir(basedir, "testing")
+        _, port_endpoint = self.port_assigner.assign(reactor)
         with open(join(basedir, "tahoe.cfg"), "w") as f:
             f.write(
                 "[node]\n"
-                "tub.location = 127.0.0.1:1\n"
-                "web.port = tcp:0\n"
+                "tub.location = 127.0.0.1:1\n" +
+                "web.port = {}\n".format(port_endpoint)
             )
 
         self.node = yield create_introducer(basedir)
