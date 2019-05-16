3a5a0fb5722ffef81ddbab086e30c808d463578b
heartsucker <heartsucker@autistici.org>
2019-05-16 16:21:12 +0200

    replaced uses of pycryptopp's ed25519 with our own

--- src/allmydata/crypto/ed25519.py
+++ src/allmydata/crypto/ed25519.py
@@ -1,8 +1,47 @@
+'''
+Ed25519 keys and helpers.
+
+Key Formatting
+--------------
+
+- in base32, keys are 52 chars long (both signing and verifying keys)
+- in base62, keys is 43 chars long
+- in base64, keys is 43 chars long
+
+We can't use base64 because we want to reserve punctuation and preserve
+cut-and-pasteability. The base62 encoding is shorter than the base32 form,
+but the minor usability improvement is not worth the documentation and
+specification confusion of using a non-standard encoding. So we stick with
+base32.
+'''
+
 import six
 
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
 from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption, \
     PublicFormat
+
+# When we were still using `pycryptopp`, BadSignatureError was the name of the exception, and now
+# that we've switched to `cryptography`, we are importing and "re-exporting" this to keep the name
+# the same (for now).
+from cryptography.exceptions import InvalidSignature
+BadSignatureError = InvalidSignature
+del InvalidSignature
+
+from allmydata.util.base32 import a2b, b2a
+
+_PRIV_PREFIX = 'priv-v0-'
+_PUB_PREFIX = 'pub-v0-'
+
+
+class BadPrefixError(Exception):
+    pass
+
+
+def _remove_prefix(s_bytes, prefix):
+    if not s_bytes.startswith(prefix):
+        raise BadPrefixError("did not see expected '%s' prefix" % (prefix,))
+    return s_bytes[len(prefix):]
 
 
 class SigningKey:
@@ -37,6 +76,24 @@ class SigningKey:
             raise ValueError('data must be bytes')
         return self._priv_key.sign(data)
 
+    @classmethod
+    def parse_encoded_key(cls, priv_str):
+        global _PRIV_PREFIX
+        return cls.from_private_bytes(a2b(_remove_prefix(priv_str, _PRIV_PREFIX)))
+
+    def encoded_key(self):
+        global _PRIV_PREFIX
+        return _PRIV_PREFIX + a2b(self.private_bytes())
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return self.private_bytes() == other.private_bytes()
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 
 class VerifyingKey:
 
@@ -62,3 +119,21 @@ class VerifyingKey:
             raise ValueError('data must be bytes')
 
         self._pub_key.verify(signature, data)
+
+    @classmethod
+    def parse_encoded_key(cls, pub_str):
+        global _PUB_PREFIX
+        return cls.from_public_bytes(a2b(_remove_prefix(pub_str, _PUB_PREFIX)))
+
+    def encoded_key(self):
+        global _PUB_PREFIX
+        return _PUB_PREFIX + a2b(self.public_bytes())
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return self.public_bytes() == other.public_bytes()
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
