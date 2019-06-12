cefa4ccbd8e6cb5dba962955022cb9894e1562a6
meejah <meejah@meejah.ca>
2019-06-12 15:44:35 -0600

    refactor away from pycryptopp "helper" classes

--- src/allmydata/crypto/rsa.py
+++ src/allmydata/crypto/rsa.py
@@ -8,110 +8,153 @@ from cryptography.hazmat.primitives.serialization import load_der_private_key, l
 from allmydata.crypto import BadSignature
 
 
-class RsaMixin(object):
+"""
+This is the value that was used by `pycryptopp`, and we must continue to use it for
+both backwards compatibility and interoperability.
 
-    '''
-    This is the value that was used by `pycryptopp`, and we must continue to use it for
-    both backwards compatibility and interoperability.
-
-    The docs for `cryptography` suggest to use the constant defined at
-    `cryptography.hazmat.primitives.asymmetric.padding.PSS.MAX_LENGTH`, but this causes old
-    signatures to fail to validate.
-    '''
-    RSA_PSS_SALT_LENGTH = 32
+The docs for `cryptography` suggest to use the constant defined at
+`cryptography.hazmat.primitives.asymmetric.padding.PSS.MAX_LENGTH`, but this causes old
+signatures to fail to validate.
+"""
+RSA_PSS_SALT_LENGTH = 32
 
 
-class PrivateKey(RsaMixin):
+def create_verifying_key_from_string(public_key_der):
+    """
+    Create an RSA verifying key from a previously serialized public key.
 
-    def __init__(self, priv_key):
-        self._priv_key = priv_key
+    :returns: public key
+    """
+    pub_key = load_der_public_key(
+        public_key_der,
+        backend=default_backend(),
+    )
+    return pub_key
 
-    @classmethod
-    def generate(cls, key_size):
-        priv_key = rsa.generate_private_key(
-            public_exponent=65537,  # serisously don't change this value
-            key_size=key_size,
-            backend=default_backend()
-        )
-        return cls(priv_key)
 
-    @classmethod
-    def parse_string(cls, priv_key_str):
-        priv_key = load_der_private_key(
-            priv_key_str,
-            password=None,
-            backend=default_backend(),
-        )
-        return cls(priv_key)
+def create_signing_keypair(key_size):
+    """
+    Create a new RSA signing keypair from scratch. Can be used with
+    `sign_data` function.
 
-    def public_key(self):
-        return PublicKey(self._priv_key.public_key())
+    :param int key_size: length of key in bits
 
-    def serialize(self):
-        return self._priv_key.private_bytes(
-            encoding=Encoding.DER,
-            format=PrivateFormat.PKCS8,
-            encryption_algorithm=NoEncryption(),
-        )
+    :returns: 2-tuple of (private_key, public_key)
+    """
+    priv_key = rsa.generate_private_key(
+        public_exponent=65537,  # serisously don't change this value
+        key_size=key_size,
+        backend=default_backend()
+    )
+    return priv_key, priv_key.public_key()
 
-    def sign(self, data):
-        return self._priv_key.sign(
+
+def create_signing_keypair_from_string(private_key_der):
+    """
+    Create an RSA signing key from a previously serialized private key.
+
+    :returns: 2-tuple of (private_key, public_key)
+    """
+    priv_key = load_der_private_key(
+        private_key_der,
+        password=None,
+        backend=default_backend(),
+    )
+    return priv_key, priv_key.public_key()
+
+
+def der_string_from_signing_key(private_key):
+    """
+    Serializes a given RSA private key to a DER string
+    """
+    _validate_private_key(private_key)
+    return private_key.private_bytes(
+        encoding=Encoding.DER,
+        format=PrivateFormat.PKCS8,
+        encryption_algorithm=NoEncryption(),
+    )
+
+
+def der_string_from_verifying_key(public_key):
+    """
+    Serializes a given RSA private key to a DER string
+    """
+    _validate_public_key(public_key)
+    return public_key.public_bytes(
+        encoding=Encoding.DER,
+        format=PublicFormat.SubjectPublicKeyInfo,
+    )
+
+
+def create_verifying_key_from_string(public_key_der):
+    """
+    Create an RSA signing key from a previously serialized public key
+    """
+    pub_key = load_der_public_key(
+        public_key_der,
+        backend=default_backend(),
+    )
+    return pub_key
+
+
+def sign_data(private_key, data):
+    """
+    :param private_key: the private part of a keypair returned from
+        `create_signing_keypair_from_string` or `create_signing_keypair`
+
+    :param bytes data: the bytes to sign
+
+    :returns: bytes which are a signature of the bytes given as `data`.
+    """
+    _validate_private_key(private_key)
+    return private_key.sign(
+        data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=RSA_PSS_SALT_LENGTH,
+        ),
+        hashes.SHA256(),
+    )
+
+def verify_signature(public_key, alleged_signature, data):
+    """
+    :param public_key: a verifying key, returned from `create_verifying_key_from_string` or `create_verifying_key_from_private_key`
+
+    :param bytes alleged_signature: the bytes of the alleged signature
+
+    :param bytes data: the data which was allegedly signed
+    """
+    try:
+        public_key.verify(
+            alleged_signature,
             data,
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=self.RSA_PSS_SALT_LENGTH,
+                salt_length=RSA_PSS_SALT_LENGTH,
             ),
             hashes.SHA256(),
         )
-
-    def __eq__(self, other):
-        if isinstance(other, type(self)):
-            return self.serialize() == other.serialize()
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
+    except InvalidSignature:
+        raise BadSignature
 
 
-class PublicKey(RsaMixin):
-
-    def __init__(self, pub_key):
-        self._pub_key = pub_key
-
-    @classmethod
-    def parse_string(cls, pub_key_str):
-        pub_key = load_der_public_key(
-            pub_key_str,
-            backend=default_backend(),
-        )
-        return cls(pub_key)
-
-    def serialize(self):
-        return self._pub_key.public_bytes(
-            encoding=Encoding.DER,
-            format=PublicFormat.SubjectPublicKeyInfo,
+def _validate_public_key(public_key):
+    """
+    Internal helper. Checks that `public_key` is a valid cryptography
+    object
+    """
+    if not isinstance(public_key, rsa.RSAPublicKey):
+        raise ValueError(
+            "public_key not an RSAPublicKey"
         )
 
-    def verify(self, signature, data):
-        try:
-            self._pub_key.verify(
-                signature,
-                data,
-                padding.PSS(
-                    mgf=padding.MGF1(hashes.SHA256()),
-                    salt_length=self.RSA_PSS_SALT_LENGTH,
-                ),
-                hashes.SHA256(),
-            )
-        except InvalidSignature:
-            raise BadSignature
 
-    def __eq__(self, other):
-        if isinstance(other, type(self)):
-            return self.serialize() == other.serialize()
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
+def _validate_private_key(private_key):
+    """
+    Internal helper. Checks that `public_key` is a valid cryptography
+    object
+    """
+    if not isinstance(private_key, rsa.RSAPrivateKey):
+        raise ValueError(
+            "private_key not an RSAPrivateKey"
+        )
--- src/allmydata/mutable/servermap.py
+++ src/allmydata/mutable/servermap.py
@@ -9,7 +9,7 @@ from twisted.python import failure
 from foolscap.api import DeadReferenceError, RemoteException, eventually, \
                          fireEventually
 from allmydata.crypto import BadSignature
-from allmydata.crypto.rsa import PublicKey, PrivateKey
+from allmydata.crypto import rsa
 from allmydata.util import base32, hashutil, log, deferredutil
 from allmydata.util.dictutil import DictOfSets
 from allmydata.storage.server import si_b2a
@@ -838,7 +838,7 @@ class ServermapUpdater(object):
             # against the public key before keeping track of it.
             assert self._node.get_pubkey()
             try:
-                self._node.get_pubkey().verify(signature[1], prefix)
+                rsa.verify_signature(self._node.get_pubkey(), signature[1], prefix)
             except BadSignature:
                 raise CorruptShareError(server, shnum,
                                         "signature is invalid")
@@ -909,7 +909,7 @@ class ServermapUpdater(object):
                                                               update_data)
 
     def _deserialize_pubkey(self, pubkey_s):
-        verifier = PublicKey.parse_string(pubkey_s)
+        verifier = rsa.create_verifying_key_from_string(pubkey_s)
         return verifier
 
     def _try_to_validate_privkey(self, enc_privkey, server, shnum, lp):
@@ -930,7 +930,7 @@ class ServermapUpdater(object):
         self.log("got valid privkey from shnum %d on serverid %s" %
                  (shnum, server.get_name()),
                  parent=lp)
-        privkey = PrivateKey.parse_string(alleged_privkey_s)
+        privkey, _ = rsa.create_signing_keypair_from_string(alleged_privkey_s)
         self._node._populate_encprivkey(enc_privkey)
         self._node._populate_privkey(privkey)
         self._need_privkey = False
