a48639baa600049179b05ac15bac55131b5f5c91
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-06-14 11:56:02 -0400

    Baseline tests for anonymous storage server announcements

--- src/allmydata/client.py
+++ src/allmydata/client.py
@@ -208,7 +208,7 @@ def create_client(basedir=u".", _client_factory=None):
         return defer.fail()
 
 
-def create_client_from_config(config, _client_factory=None):
+def create_client_from_config(config, _client_factory=None, introducer_factory=None):
     """
     Creates a new client instance (a subclass of Node).  Most code
     should probably use `create_client` instead.
@@ -220,6 +220,9 @@ def create_client_from_config(config, _client_factory=None):
 
     :param _client_factory: for testing; the class to instantiate
         instead of _Client
+
+    :param introducer_factory: for testing; the class to instantiate instead
+        of IntroducerClient
     """
     try:
         if _client_factory is None:
@@ -237,7 +240,7 @@ def create_client_from_config(config, _client_factory=None):
         )
         control_tub = node.create_control_tub()
 
-        introducer_clients = create_introducer_clients(config, main_tub)
+        introducer_clients = create_introducer_clients(config, main_tub, introducer_factory)
         storage_broker = create_storage_farm_broker(
             config, default_connection_handlers, foolscap_connection_handlers,
             tub_options, introducer_clients
@@ -279,12 +282,18 @@ def _sequencer(config):
     return seqnum, nonce
 
 
-def create_introducer_clients(config, main_tub):
+def create_introducer_clients(config, main_tub, introducer_factory=None):
     """
     Read, validate and parse any 'introducers.yaml' configuration.
 
+    :param introducer_factory: for testing; the class to instantiate instead
+        of IntroducerClient
+
     :returns: a list of IntroducerClient instances
     """
+    if introducer_factory is None:
+        introducer_factory = IntroducerClient
+
     # we return this list
     introducer_clients = []
 
@@ -330,7 +339,7 @@ def create_introducer_clients(config, main_tub):
 
     for petname, introducer in introducers.items():
         introducer_cache_filepath = FilePath(config.get_private_path("introducer_{}_cache.yaml".format(petname)))
-        ic = IntroducerClient(
+        ic = introducer_factory(
             main_tub,
             introducer['furl'].encode("ascii"),
             config.nickname,
