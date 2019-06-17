f7caf77c526675c2cb99122322a774a9996f95a9
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-06-17 16:44:17 -0400

    Allow for dynamic configuration validation rules

--- src/allmydata/test/test_configutil.py
+++ src/allmydata/test/test_configutil.py
@@ -9,6 +9,16 @@ from .. import client
 
 
 class ConfigUtilTests(GridTestMixin, unittest.TestCase):
+    def setUp(self):
+        super(ConfigUtilTests, self).setUp()
+        self.static_valid_config = configutil.ValidConfiguration(
+            dict(node=['valid']),
+        )
+        self.dynamic_valid_config = configutil.ValidConfiguration(
+            dict(),
+            lambda section_name: section_name == "node",
+            lambda section_name, item_name: (section_name, item_name) == ("node", "valid"),
+        )
 
     def test_config_utils(self):
         self.basedir = "cli/ConfigUtilTests/test-config-utils"
@@ -44,7 +54,32 @@ class ConfigUtilTests(GridTestMixin, unittest.TestCase):
 
         config = configutil.get_config(fname)
         # should succeed, no exceptions
-        configutil.validate_config(fname, config, dict(node=['valid']))
+        configutil.validate_config(
+            fname,
+            config,
+            self.static_valid_config,
+        )
+
+    def test_config_dynamic_validation_success(self):
+        """
+        A configuration with sections and items that are not matched by the static
+        validation but are matched by the dynamic validation is considered
+        valid.
+        """
+        d = self.mktemp()
+        os.mkdir(d)
+        fname = os.path.join(d, 'tahoe.cfg')
+
+        with open(fname, 'w') as f:
+            f.write('[node]\nvalid = foo\n')
+
+        config = configutil.get_config(fname)
+        # should succeed, no exceptions
+        configutil.validate_config(
+            fname,
+            config,
+            self.dynamic_valid_config,
+        )
 
     def test_config_validation_invalid_item(self):
         d = self.mktemp()
@@ -58,11 +93,16 @@ class ConfigUtilTests(GridTestMixin, unittest.TestCase):
         e = self.assertRaises(
             configutil.UnknownConfigError,
             configutil.validate_config,
-            fname, config, dict(node=['valid']),
+            fname, config,
+            self.static_valid_config,
         )
         self.assertIn("section [node] contains unknown option 'invalid'", str(e))
 
     def test_config_validation_invalid_section(self):
+        """
+        A configuration with a section that is matched by neither the static nor
+        dynamic validators is rejected.
+        """
         d = self.mktemp()
         os.mkdir(d)
         fname = os.path.join(d, 'tahoe.cfg')
@@ -74,9 +114,52 @@ class ConfigUtilTests(GridTestMixin, unittest.TestCase):
         e = self.assertRaises(
             configutil.UnknownConfigError,
             configutil.validate_config,
-            fname, config, dict(node=['valid']),
+            fname, config,
+            self.static_valid_config,
         )
         self.assertIn("contains unknown section [invalid]", str(e))
+
+    def test_config_dynamic_validation_invalid_section(self):
+        """
+        A configuration with a section that is matched by neither the static nor
+        dynamic validators is rejected.
+        """
+        d = self.mktemp()
+        os.mkdir(d)
+        fname = os.path.join(d, 'tahoe.cfg')
+
+        with open(fname, 'w') as f:
+            f.write('[node]\nvalid = foo\n[invalid]\n')
+
+        config = configutil.get_config(fname)
+        e = self.assertRaises(
+            configutil.UnknownConfigError,
+            configutil.validate_config,
+            fname, config,
+            self.dynamic_valid_config,
+        )
+        self.assertIn("contains unknown section [invalid]", str(e))
+
+    def test_config_dynamic_validation_invalid_item(self):
+        """
+        A configuration with a section, item pair that is matched by neither the
+        static nor dynamic validators is rejected.
+        """
+        d = self.mktemp()
+        os.mkdir(d)
+        fname = os.path.join(d, 'tahoe.cfg')
+
+        with open(fname, 'w') as f:
+            f.write('[node]\nvalid = foo\ninvalid = foo\n')
+
+        config = configutil.get_config(fname)
+        e = self.assertRaises(
+            configutil.UnknownConfigError,
+            configutil.validate_config,
+            fname, config,
+            self.dynamic_valid_config,
+        )
+        self.assertIn("section [node] contains unknown option 'invalid'", str(e))
 
     def test_create_client_config(self):
         d = self.mktemp()
@@ -97,5 +180,8 @@ class ConfigUtilTests(GridTestMixin, unittest.TestCase):
 
         config = configutil.get_config(fname)
         # should succeed, no exceptions
-        configutil.validate_config(fname, config,
-                                   client._valid_config_sections())
+        configutil.validate_config(
+            fname,
+            config,
+            client._valid_config(),
+        )
