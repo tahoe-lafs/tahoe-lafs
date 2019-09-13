bfa2cb8816af3ee0e36668a7fd93c3ce0dd93432
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-13 12:50:05 -0400

    Close paren needed

--- misc/python3/depgraph.sh
+++ misc/python3/depgraph.sh
@@ -30,7 +30,7 @@ fi
 git commit -am "\
 Built from ${CIRCLE_REPOSITORY_URL}@${CIRCLE_SHA1}
 
-tahoe-depgraph was $(git --git-dir ../.git rev-parse HEAD
+tahoe-depgraph was $(git --git-dir ../.git rev-parse HEAD)
 "
 
 # Publish it on GitHub.
