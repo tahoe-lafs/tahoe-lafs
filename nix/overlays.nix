bebc0dd193c6f535585c56be247d3fe14dfdaa9e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-12-10 15:32:12 -0500

    use our nevow :/ :\ :/ :\ :/ :\ :/

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -2,7 +2,7 @@ self: super: {
   python27 = super.python27.override {
     packageOverrides = python-self: python-super: {
       eliot = python-self.callPackage ./eliot.nix { };
-      nevow = python-super.nevow.overrideAttrs (old: { doCheck = false; }); # callPackage ./nevow.nix { };
+      nevow = python-super.callPackage ./nevow.nix { };
     };
   };
 }
