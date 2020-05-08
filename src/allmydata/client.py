0bdfae845ed623f5e926dbef1041a6606b4f6bb9
meejah <meejah@meejah.ca>
2020-05-07 22:30:05 -0600

    actually put grid-manager-certificates in announcement

--- src/allmydata/client.py
+++ src/allmydata/client.py
@@ -943,10 +943,9 @@ class _Client(node.Node, pollmixin.PollMixin):
 
         announcement.update(plugins_announcement)
 
-        grid_manager_certificates = []
-
         if self.config.get_config("storage", "grid_management", default=False, boolean=True):
             grid_manager_certificates = _load_grid_manager_certificates(self.config)
+            announcement["grid-manager-certificates"] = grid_manager_certificates
 
         # XXX we should probably verify that the certificates are
         # valid and not expired, as that could be confusing for the
