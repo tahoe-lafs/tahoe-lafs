26d7a3a957faf35520ce349d235da840fa8b2c15
meejah <meejah@meejah.ca>
2020-06-19 17:15:01 -0600

    Get rid of ?replace= handling entirely and more-correctly support /uri variants

--- src/allmydata/test/test_testing.py
+++ src/allmydata/test/test_testing.py
@@ -21,6 +21,11 @@ from allmydata.uri import (
 )
 from allmydata.testing.web import (
     create_tahoe_treq_client,
+    capability_generator,
+)
+
+from hyperlink import (
+    DecodedURL,
 )
 
 from hypothesis import (
@@ -35,9 +40,12 @@ from testtools import (
 )
 from testtools.matchers import (
     Always,
+    Equals,
+    MatchesStructure,
 )
 from testtools.twistedsupport import (
     succeeded,
+    failed,
 )
 
 
@@ -46,9 +54,9 @@ class FakeWebTest(TestCase):
     Test the WebUI verified-fakes infrastucture
     """
 
-    def setUp(self):
-        super(FakeWebTest, self).setUp()
-        self.http_client = create_tahoe_treq_client()
+    # Note: do NOT use setUp() because Hypothesis doesn't work
+    # properly with it. You must instead do all fixture-type work
+    # yourself in each test.
 
     @given(
         content=binary(),
@@ -58,18 +66,28 @@ class FakeWebTest(TestCase):
         Upload some content (via 'PUT /uri') and then download it (via
         'GET /uri?uri=...')
         """
+        http_client = create_tahoe_treq_client()
 
         @inlineCallbacks
         def do_test():
-            resp = yield self.http_client.put("http://example.com/uri?replace=true", content)
+            resp = yield http_client.put("http://example.com/uri", content)
             self.assertEqual(resp.code, 201)
 
             cap_raw = yield resp.content()
             cap = from_string(cap_raw)
             self.assertIsInstance(cap, CHKFileURI)
 
-            resp = yield self.http_client.get(
+            resp = yield http_client.get(
                 "http://example.com/uri?uri={}".format(cap.to_string())
+            )
+            self.assertEqual(resp.code, 200)
+
+            round_trip_content = yield resp.content()
+
+            # using the form "/uri/<cap>" is also valid
+
+            resp = yield http_client.get(
+                "http://example.com/uri/{}".format(cap.to_string())
             )
             self.assertEqual(resp.code, 200)
 
@@ -80,21 +98,67 @@ class FakeWebTest(TestCase):
             succeeded(Always()),
         )
 
-    @inlineCallbacks
-    def test_duplicate_upload(self):
+    @given(
+        content=binary(),
+    )
+    def test_duplicate_upload(self, content):
         """
-        Upload the same content (via 'PUT /uri') twice with no overwrite
+        Upload the same content (via 'PUT /uri') twice
         """
 
-        content = "fake content\n" * 200
+        http_client = create_tahoe_treq_client()
 
-        resp = yield self.http_client.put("http://example.com/uri", content)
-        self.assertEqual(resp.code, 201)
+        @inlineCallbacks
+        def do_test():
+            resp = yield http_client.put("http://example.com/uri", content)
+            self.assertEqual(resp.code, 201)
 
-        cap_raw = yield resp.content()
-        cap = from_string(cap_raw)
-        self.assertIsInstance(cap, CHKFileURI)
+            cap_raw = yield resp.content()
+            cap = from_string(cap_raw)
+            self.assertIsInstance(cap, CHKFileURI)
 
-        # this one fails: same content but no ?replace=true
-        resp = yield self.http_client.put("http://example.com/uri", content)
-        self.assertEqual(resp.code, 409)
+            resp = yield http_client.put("http://example.com/uri", content)
+            self.assertThat(resp.code, Equals(200))
+        self.assertThat(
+            do_test(),
+            succeeded(Always()),
+        )
+
+    def test_download_missing(self):
+        """
+        Error if we download a capability that doesn't exist
+        """
+
+        http_client = create_tahoe_treq_client()
+        cap_gen = capability_generator("URI:CHK:")
+
+        uri = DecodedURL.from_text(u"http://example.com/uri?uri={}".format(next(cap_gen)))
+        resp = http_client.get(uri.to_uri().to_text())
+
+        self.assertThat(
+            resp,
+            succeeded(
+                MatchesStructure(
+                    code=Equals(500)
+                )
+            )
+        )
+
+    def test_download_no_arg(self):
+        """
+        Error if we GET from "/uri" with no ?uri= query-arg
+        """
+
+        http_client = create_tahoe_treq_client()
+
+        uri = DecodedURL.from_text(u"http://example.com/uri/")
+        resp = http_client.get(uri.to_uri().to_text())
+
+        self.assertThat(
+            resp,
+            succeeded(
+                MatchesStructure(
+                    code=Equals(400)
+                )
+            )
+        )
