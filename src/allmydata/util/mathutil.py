939648c04fbcde890a90edde10d853f00e0fdbc0
Itamar Turner-Trauring <itamar@itamarst.org>
2020-07-08 12:13:19 -0400

    Add back missing function.

--- src/allmydata/util/mathutil.py
+++ src/allmydata/util/mathutil.py
@@ -17,4 +17,11 @@ if PY2:
 
 
 # The API importers expect:
-from pyutil.mathutil import div_ceil, next_multiple, pad_size, is_power_of_k, next_power_of_k, ave, log_ceil, log_floor, round_sigfigs
+from pyutil.mathutil import div_ceil, next_multiple, pad_size, is_power_of_k, next_power_of_k, ave, log_ceil, log_floor
+
+
+# This function is not present in pyutil.mathutil:
+def round_sigfigs(f, n):
+    fmt = "%." + str(n-1) + "e"
+    return float(fmt % f)
+
