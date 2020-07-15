2b896740524cfd465e5b867084974cf363b03d7a
Itamar Turner-Trauring <itamar@itamarst.org>
2020-07-15 10:59:14 -0400

    Everything must be bytes.

--- src/allmydata/test/test_netstring.py
+++ src/allmydata/test/test_netstring.py
@@ -1,36 +1,41 @@
+"""
+Tests for allmydata.util.netstring.
+"""
 
 from twisted.trial import unittest
+
 from allmydata.util.netstring import netstring, split_netstring
+
 
 class Netstring(unittest.TestCase):
     def test_split(self):
-        a = netstring("hello") + netstring("world")
-        self.failUnlessEqual(split_netstring(a, 2), (["hello", "world"], len(a)))
-        self.failUnlessEqual(split_netstring(a, 2, required_trailer=""), (["hello", "world"], len(a)))
+        a = netstring(b"hello") + netstring(b"world")
+        self.failUnlessEqual(split_netstring(a, 2), ([b"hello", b"world"], len(a)))
+        self.failUnlessEqual(split_netstring(a, 2, required_trailer=""), ([b"hello", b"world"], len(a)))
         self.failUnlessRaises(ValueError, split_netstring, a, 3)
-        self.failUnlessRaises(ValueError, split_netstring, a+" extra", 2, required_trailer="")
-        self.failUnlessEqual(split_netstring(a+" extra", 2), (["hello", "world"], len(a)))
-        self.failUnlessEqual(split_netstring(a+"++", 2, required_trailer="++"),
-                             (["hello", "world"], len(a)+2))
+        self.failUnlessRaises(ValueError, split_netstring, a+b" extra", 2, required_trailer="")
+        self.failUnlessEqual(split_netstring(a+b" extra", 2), ([b"hello", b"world"], len(a)))
+        self.failUnlessEqual(split_netstring(a+b"++", 2, required_trailer=b"++"),
+                             ([b"hello", b"world"], len(a)+2))
         self.failUnlessRaises(ValueError,
-                              split_netstring, a+"+", 2, required_trailer="not")
+                              split_netstring, a+b"+", 2, required_trailer=b"not")
 
     def test_extra(self):
-        a = netstring("hello")
-        self.failUnlessEqual(split_netstring(a, 1), (["hello"], len(a)))
-        b = netstring("hello") + "extra stuff"
+        a = netstring(b"hello")
+        self.failUnlessEqual(split_netstring(a, 1), ([b"hello"], len(a)))
+        b = netstring(b"hello") + b"extra stuff"
         self.failUnlessEqual(split_netstring(b, 1),
-                             (["hello"], len(a)))
+                             ([b"hello"], len(a)))
 
     def test_nested(self):
-        a = netstring("hello") + netstring("world") + "extra stuff"
-        b = netstring("a") + netstring("is") + netstring(a) + netstring(".")
+        a = netstring(b"hello") + netstring(b"world") + b"extra stuff"
+        b = netstring(b"a") + netstring(b"is") + netstring(a) + netstring(b".")
         (top, pos) = split_netstring(b, 4)
         self.failUnlessEqual(len(top), 4)
-        self.failUnlessEqual(top[0], "a")
-        self.failUnlessEqual(top[1], "is")
+        self.failUnlessEqual(top[0], b"a")
+        self.failUnlessEqual(top[1], b"is")
         self.failUnlessEqual(top[2], a)
-        self.failUnlessEqual(top[3], ".")
-        self.failUnlessRaises(ValueError, split_netstring, a, 2, required_trailer="")
+        self.failUnlessEqual(top[3], b".")
+        self.failUnlessRaises(ValueError, split_netstring, a, 2, required_trailer=b"")
         bottom = split_netstring(a, 2)
-        self.failUnlessEqual(bottom, (["hello", "world"], len(netstring("hello")+netstring("world"))))
+        self.failUnlessEqual(bottom, ([b"hello", b"world"], len(netstring(b"hello")+netstring(b"world"))))
