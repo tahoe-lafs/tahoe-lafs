9919d2c9a7fcb2887541d887d94daf2db8b08275
Itamar Turner-Trauring <itamar@itamarst.org>
2020-07-27 11:46:03 -0400

    Record port status.

--- src/allmydata/test/test_iputil.py
+++ src/allmydata/test/test_iputil.py
@@ -1,3 +1,9 @@
+"""
+Tests for allmydata.util.iputil.
+
+Ported to Python 3.
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
--- src/allmydata/util/iputil.py
+++ src/allmydata/util/iputil.py
@@ -1,3 +1,9 @@
+"""
+Utilities for getting IP addresses.
+
+Ported to Python 3.
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
