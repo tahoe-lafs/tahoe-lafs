70851fe75384ab3d7015480329ab6995372b7896
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-05 11:53:23 -0400

    Death to newint.

--- src/allmydata/util/_python3.py
+++ src/allmydata/util/_python3.py
@@ -11,7 +11,7 @@ from __future__ import print_function
 
 from future.utils import PY2
 if PY2:
-    from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, int, list, object, range, str, max, min  # noqa: F401
+    from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 
 # Keep these sorted alphabetically, to reduce merge conflicts:
 PORTED_MODULES = [
