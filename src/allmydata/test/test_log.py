32945b85f6d84820843465f39da732ad5170fdde
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-06 16:14:39 -0400

    Drop int.

--- src/allmydata/test/test_log.py
+++ src/allmydata/test/test_log.py
@@ -11,7 +11,7 @@ from __future__ import print_function
 
 from future.utils import PY2
 if PY2:
-    from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, int, list, object, range, str, max, min  # noqa: F401
+    from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 
 from twisted.trial import unittest
 from twisted.python.failure import Failure
