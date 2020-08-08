ef3cbebe431b9978d211434dc6c141d3c26699eb
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-08-07 22:57:59 -0400

    Perhaps get this path right

--- misc/python3/ratchet.sh
+++ misc/python3/ratchet.sh
@@ -11,8 +11,8 @@ cd "../.."
 
 # Since both of the next calls are expected to exit non-0, relax our guard.
 set +e
-SUBUNITREPORTER_OUTPUT_PATH="$base/results.subunit2" trial --temp-directory /tmp/_trial_temp.ratchet --reporter subunitv2-file allmydata
-subunit2junitxml < "$base/results.subunit2" > "$base/results.xml"
+trial --temp-directory /tmp/_trial_temp.ratchet --reporter subunitv2-file allmydata
+subunit2junitxml < "${SUBUNITREPORTER_OUTPUT_PATH}" > "$base/results.xml"
 set -e
 
 # Okay, now we're clear.
