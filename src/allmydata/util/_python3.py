243d02ecb1a01216fc16331ec25a04186dfce063
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-11 14:08:51 -0400

    Port to Python 3.

--- src/allmydata/util/_python3.py
+++ src/allmydata/util/_python3.py
@@ -50,6 +50,7 @@ PORTED_TEST_MODULES = [
     "allmydata.test.test_abbreviate",
     "allmydata.test.test_base32",
     "allmydata.test.test_base62",
+    "allmydata.test.test_connection_status",
     "allmydata.test.test_crypto",
     "allmydata.test.test_deferredutil",
     "allmydata.test.test_dictutil",
