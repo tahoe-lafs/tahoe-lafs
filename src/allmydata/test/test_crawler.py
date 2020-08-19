733b2cab361879aaf5ce0cffa73b5ef275645a29
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-19 13:26:57 -0400

    Remove unnecessary import.

--- src/allmydata/test/test_crawler.py
+++ src/allmydata/test/test_crawler.py
@@ -9,7 +9,7 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from future.utils import PY2, PY3, native_str
+from future.utils import PY2, PY3
 if PY2:
     # Don't use future bytes, since it breaks tests.
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, dict, list, object, range, str, max, min  # noqa: F401
