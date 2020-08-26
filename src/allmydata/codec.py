b3460dcddc61e4b838239e8a2137ef6182f59327
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-26 11:00:19 -0400

    Fix lint.

--- src/allmydata/codec.py
+++ src/allmydata/codec.py
@@ -11,7 +11,6 @@ from __future__ import unicode_literals
 from future.utils import PY2
 if PY2:
     from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
-from future.utils import native_str
 
 from zope.interface import implementer
 from twisted.internet import defer
