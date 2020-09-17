03fd566e2c1f9d25332643d8d71de8f85b2533d1
Itamar Turner-Trauring <itamar@itamarst.org>
2020-09-17 12:37:10 -0400

    Fix flake error.

--- src/allmydata/storage/server.py
+++ src/allmydata/storage/server.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from future.utils import bytes_to_native_str, PY2, native_str_to_bytes
+from future.utils import bytes_to_native_str, PY2
 if PY2:
     # Omit open() to get native behavior where open("w") always accepts native
     # strings. Omit bytes so we don't leak future's custom bytes.
