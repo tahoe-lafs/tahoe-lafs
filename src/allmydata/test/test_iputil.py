96c848b2ad7413ef0a86bdb85f9675edbbbbc4a5
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-10-23 09:50:31 -0400

    flakes

--- src/allmydata/test/test_iputil.py
+++ src/allmydata/test/test_iputil.py
@@ -13,7 +13,7 @@ from future.utils import PY2, native_str
 if PY2:
     from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 
-import re, os, socket
+import os, socket
 import gc
 
 from testtools.matchers import (
