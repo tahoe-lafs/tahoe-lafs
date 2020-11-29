13cd780231bf3185104d930d748c75b9d324eabc
Jason R. Coombs <jaraco@jaraco.com>
2020-11-29 16:22:12 -0500

    Prefer sys.maxsize to sys.maxint.

--- src/allmydata/test/check_memory.py
+++ src/allmydata/test/check_memory.py
@@ -499,13 +499,13 @@ if __name__ == '__main__':
     mode = "upload"
     if len(sys.argv) > 1:
         mode = sys.argv[1]
-    if sys.maxint == 2147483647:
+    if sys.maxsize == 2147483647:
         bits = "32"
-    elif sys.maxint == 9223372036854775807:
+    elif sys.maxsize == 9223372036854775807:
         bits = "64"
     else:
         bits = "?"
-    print("%s-bit system (sys.maxint=%d)" % (bits, sys.maxint))
+    print("%s-bit system (sys.maxsize=%d)" % (bits, sys.maxsize))
     # put the logfile and stats.out in _test_memory/ . These stick around.
     # put the nodes and other files in _test_memory/test/ . These are
     # removed each time we run.
