c1a699dede317cd6ef60613f97e1aa90b1355d4f
Itamar Turner-Trauring <itamar@itamarst.org>
2020-12-04 11:39:57 -0500

    Flake.

--- src/allmydata/introducer/server.py
+++ src/allmydata/introducer/server.py
@@ -12,7 +12,7 @@ from future.utils import PY2
 if PY2:
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 from past.builtins import long
-from six import ensure_str, ensure_text
+from six import ensure_text
 
 import time, os.path, textwrap
 from zope.interface import implementer
