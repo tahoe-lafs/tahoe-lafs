b77f43e360ea3b71ef2552ba237767c20eda7483
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-12-14 18:27:02 -0500

    Simplify handler initialization

--- src/allmydata/node.py
+++ src/allmydata/node.py
@@ -669,8 +669,6 @@ def create_connection_handlers(config, i2p_provider, tor_provider):
     # create that handler, so hints which want it will be ignored.
     handlers = {
         "tcp": _make_tcp_handler(),
-    }
-    handlers.update({
         "tor": tor_provider.get_tor_handler(),
         "i2p": i2p_provider.get_i2p_handler(),
     })
