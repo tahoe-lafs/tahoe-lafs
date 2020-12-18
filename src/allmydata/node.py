189608e11388988b512aa00f225911ff61afc80c
Jason R. Coombs <jaraco@jaraco.com>
2020-12-18 11:07:30 -0500

    Remove GENERATED_FILES, unused

--- src/allmydata/node.py
+++ src/allmydata/node.py
@@ -713,7 +713,6 @@ class Node(service.MultiService):
     """
     NODETYPE = "unknown NODETYPE"
     CERTFILE = "node.pem"
-    GENERATED_FILES = []  # type: ignore
 
     def __init__(self, config, main_tub, control_tub, i2p_provider, tor_provider):
         """
