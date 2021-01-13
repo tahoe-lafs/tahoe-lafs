c5669e16e0ba0943e83fd7f3e39a2569eb582743
Itamar Turner-Trauring <itamar@itamarst.org>
2021-01-13 09:56:08 -0500

    Fix flake.

--- src/allmydata/test/web/test_private.py
+++ src/allmydata/test/web/test_private.py
@@ -15,8 +15,6 @@ from future.utils import PY2
 if PY2:
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 
-from future.builtins import str
-
 from testtools.matchers import (
     Equals,
 )
