2bd244dde95d32a0949abfd465d06428b56179d9
Jason R. Coombs <jaraco@jaraco.com>
2021-01-30 11:24:06 -0500

    Declare scripts.runner as ported. Ref #3603.

--- src/allmydata/util/_python3.py
+++ src/allmydata/util/_python3.py
@@ -72,6 +72,7 @@ PORTED_MODULES = [
     "allmydata.mutable.servermap",
     "allmydata.node",
     "allmydata.nodemaker",
+    "allmydata.scripts.runner",
     "allmydata.scripts.types_",
     "allmydata.stats",
     "allmydata.storage_client",
