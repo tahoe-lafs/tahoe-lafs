a06956632b1eb19a2f136ea6a2bd0b9d10dace93
Itamar Turner-Trauring <itamar@itamarst.org>
2021-02-12 11:37:43 -0500

    More unicode-of-bytes bug fixes.

--- src/allmydata/storage/server.py
+++ src/allmydata/storage/server.py
@@ -271,7 +271,7 @@ class StorageServer(service.MultiService, Referenceable):
         si_dir = storage_index_to_dir(storage_index)
         si_s = si_b2a(storage_index)
 
-        log.msg("storage: allocate_buckets %s" % si_s)
+        log.msg("storage: allocate_buckets %r" % si_s)
 
         # in this implementation, the lease information (including secrets)
         # goes into the share files themselves. It could also be put into a
@@ -397,7 +397,7 @@ class StorageServer(service.MultiService, Referenceable):
         start = time.time()
         self.count("get")
         si_s = si_b2a(storage_index)
-        log.msg("storage: get_buckets %s" % si_s)
+        log.msg("storage: get_buckets %r" % si_s)
         bucketreaders = {} # k: sharenum, v: BucketReader
         for shnum, filename in self._get_bucket_shares(storage_index):
             bucketreaders[shnum] = BucketReader(self, filename,
@@ -602,7 +602,7 @@ class StorageServer(service.MultiService, Referenceable):
         start = time.time()
         self.count("writev")
         si_s = si_b2a(storage_index)
-        log.msg("storage: slot_writev %s" % si_s)
+        log.msg("storage: slot_writev %r" % si_s)
         si_dir = storage_index_to_dir(storage_index)
         (write_enabler, renew_secret, cancel_secret) = secrets
         bucketdir = os.path.join(self.sharedir, si_dir)
@@ -669,7 +669,7 @@ class StorageServer(service.MultiService, Referenceable):
         start = time.time()
         self.count("readv")
         si_s = si_b2a(storage_index)
-        lp = log.msg("storage: slot_readv %s %s" % (si_s, shares),
+        lp = log.msg("storage: slot_readv %r %r" % (si_s, shares),
                      facility="tahoe.storage", level=log.OPERATIONAL)
         si_dir = storage_index_to_dir(storage_index)
         # shares exist if there is a file for them
