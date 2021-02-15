tahoe-lafs/tahoe-lafs

commit d02334bfd5ac8a1ec72943b01afd18cf33929fc2

Rely on futurize to expose the moved modules.

src/allmydata/scripts/common.py
@@ -5,8 +5,7 @@ from __future__ import print_function
 import os, sys, textwrap
 import codecs
 from os.path import join
-
-from six.moves import urllib  # import urllib.parse
+import urllib.parse
 
 try:
     from typing import Optional

commit 939988a042116f85c8d9027086633b9ff2cd0f67

Add workaround for compatibility on Python 2 where test.cli.test_create_alias expects the URL to be a byte string, broken in d02334bfd5ac.

src/allmydata/scripts/common.py
@@ -285,4 +285,8 @@ def escape_path(path):
     True
     """
     segments = path.split("/")
-    return "/".join([urllib.parse.quote(unicode_to_url(s)) for s in segments])
+    result = "/".join([urllib.parse.quote(unicode_to_url(s)) for s in segments])
+    # fixme: test.cli.test_create_alias fails if it gets Unicode on Python 2
+    if PY2 and isinstance(result, type(u'')):
+        result = result.encode('ascii')
+    return result