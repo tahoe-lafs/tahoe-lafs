2ad8a4745519eac032cee4d2f46189cc3fa117b3
Itamar Turner-Trauring <itamar@itamarst.org>
2021-04-26 09:59:18 -0400

    Fix flake.

--- src/allmydata/test/cli/test_cp.py
+++ src/allmydata/test/cli/test_cp.py
@@ -9,7 +9,6 @@ from __future__ import unicode_literals
 from future.utils import PY2
 if PY2:
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
-from six import ensure_text
 
 import os.path, json
 from twisted.trial import unittest
