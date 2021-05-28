b509ff69fcb77088990ff8126da6e9a8c8485374
Itamar Turner-Trauring <itamar@itamarst.org>
2021-05-28 11:25:16 -0400

    Flake fix.

--- src/allmydata/test/cli/test_backup.py
+++ src/allmydata/test/cli/test_backup.py
@@ -6,16 +6,14 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from future.utils import PY2, PY3
+from future.utils import PY2
 if PY2:
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
-from six import ensure_str
 
 import os.path
 from six.moves import cStringIO as StringIO
 from datetime import timedelta
 import re
-import locale
 
 from twisted.trial import unittest
 from twisted.python.monkey import MonkeyPatcher
