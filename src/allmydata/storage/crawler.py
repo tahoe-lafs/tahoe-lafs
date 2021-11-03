23ff1b2430334d376abd426421039562f94bcfc8
meejah <meejah@meejah.ca>
2021-11-02 22:45:08 -0600

    noqa

--- src/allmydata/storage/crawler.py
+++ src/allmydata/storage/crawler.py
@@ -11,7 +11,7 @@ from __future__ import print_function
 
 from future.utils import PY2, PY3
 if PY2:
-    from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min
+    from builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 
 import os
 import time
