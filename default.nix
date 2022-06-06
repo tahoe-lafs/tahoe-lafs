3e234d0f232e4eceb8929190c46e08f0dc500478
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-06-06 16:27:04 -0400

    Adapt to flakes

--- default.nix
+++ default.nix
@@ -20,42 +20,41 @@ let
   # https://github.com/nmattia/niv
   sources = import nix/sources.nix;
 in
-{
-  pkgsVersion ? "nixpkgs-21.11" # a string which chooses a nixpkgs from the
-                                # niv-managed sources data
+{ pkgs
+# a list of strings identifying tahoe-lafs extras, the dependencies of which
+# the resulting package will also depend on.  Include all of the runtime
+# extras by default because the incremental cost of including them is a lot
+# smaller than the cost of re-building the whole thing to add them.
+, extras ? [ "tor" "i2p" ]
 
-, pkgs ? import sources.${pkgsVersion} { } # nixpkgs itself
+# the mach-nix builder to use to build the tahoe-lafs package
+, buildPythonPackage
 
-, pypiData ? sources.pypi-deps-db # the pypi package database snapshot to use
-                                  # for dependency resolution
-
-, pythonVersion ? "python37" # a string choosing the python derivation from
-                             # nixpkgs to target
-
-, extras ? [ "tor" "i2p" ] # a list of strings identifying tahoe-lafs extras,
-                           # the dependencies of which the resulting package
-                           # will also depend on.  Include all of the runtime
-                           # extras by default because the incremental cost of
-                           # including them is a lot smaller than the cost of
-                           # re-building the whole thing to add them.
-
-, mach-nix ? import sources.mach-nix { # the mach-nix package to use to build
-                                       # the tahoe-lafs package
-    inherit pkgs pypiData;
-    python = pythonVersion;
-}
+# The name of the Python derivation in nixpkgs for which to build the package.
+, pythonVersion
 }:
 # The project name, version, and most other metadata are automatically
 # extracted from the source.  Some requirements are not properly extracted
 # and those cases are handled below.  The version can only be extracted if
 # `setup.py update_version` has been run (this is not at all ideal but it
 # seems difficult to fix) - so for now just be sure to run that first.
-mach-nix.buildPythonPackage rec {
+buildPythonPackage rec {
   # Define the location of the Tahoe-LAFS source to be packaged.  Clean up all
   # as many of the non-source files (eg the `.git` directory, `~` backup
   # files, nix's own `result` symlink, etc) as possible to avoid needing to
   # re-build when files that make no difference to the package have changed.
   src = pkgs.lib.cleanSource ./.;
+
+  python = pythonVersion;
+
+  # New-style `nix build` cannot find `src/allmydata/_version.py` because it
+  # is not version controlled.  This is not related to the `cleanSource`
+  # above.  It seems like new-style builds ignore non-version controlled
+  # sources in all cases.  As a result, mach-nix cannot infer the version of
+  # this source and the build fails - unless we specify the version here.
+  #
+  # There is probably another way to fix this.
+  version = "1.17.1.post0";
 
   # Select whichever package extras were requested.
   inherit extras;
