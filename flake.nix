bf4e4271cd04ac76040534e420f05292dbcbae82
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-06-06 17:27:58 -0400

    Factor most of the hard parts into a separate library

--- flake.nix
+++ flake.nix
@@ -20,67 +20,29 @@
   outputs = { self, nixpkgs, flake-utils, mach-nix }:
     flake-utils.lib.eachDefaultSystem (system: let
 
+      lib = import ./lib.nix {
+        inherit system pkgs mach-nix;
+      };
+      inherit (lib) packageForVersions devShellForVersions withDefault;
+
       pkgs = nixpkgs.legacyPackages.${system};
 
-      # The Python version used by the default package
+      # the Python version used by the default package
       defaultPythonVersion = "python37";
 
-      # The Python versions for which packages are available
+      # the Python versions for which packages are available
       supportedPythonVersions = ["python37" "python38" "python39" "python310" ];
 
-      # Construct the name of the package for a given version of Python
-      # str -> str
-      packageName = pythonVersion: "tahoe-lafs-${pythonVersion}";
-
-      packageForVersion = pkgs: extras: pyVersion:
-        pkgs.callPackage ./default.nix {
-          inherit extras;
-          inherit (mach-nix.lib.${system}) buildPythonPackage;
-          pythonVersion = pyVersion;
-        };
-
-      # Create packages for all of the given Python versions.
-      #
-      # nixpkgs -> derivation -> [str] -> set
-      packageForVersions = pkgs: pythonVersions:
-        let
-          mkPackage = packageForVersion pkgs [ "tor" "i2p" ];
-        in
-          builtins.foldl' (accum: pyVersion: accum // {
-            ${packageName pyVersion} = mkPackage pyVersion;
-          }) {} pythonVersions;
-
-      devShellForVersion = pkgs: extras: pyVersion:
-        pkgs.mkShell {
-          shellHook = "export PYTHONPATH=\${PWD}/src:\${PYTHONPATH}";
-          buildInputs = [ (devPy pkgs extras pyVersion) ];
-        };
-
-      devShellForVersions = pkgs: pythonVersions:
-        let
-          mkPackage = devShellForVersion pkgs [ "tor" "i2p" "test" ];
-        in
-          builtins.foldl' (accum: pyVersion: accum // {
-            ${packageName pyVersion} = mkPackage pyVersion;
-          }) {} pythonVersions;
-
-      devPy = pkgs: extras: pythonVersion:
-        mach-nix.lib.${system}.mkPython {
-          requirements = requirementsForVersion pkgs extras pythonVersion;
-        };
-
-      requirementsForVersion = pkgs: extras: pyVersion: (
-        packageForVersion pkgs extras pyVersion
-      ).requirements;
+      # the extras we will include in all packages
+      extras = [ "tor" "i2p" ];
 
     in rec {
       version = builtins.substring 0 8 self.lastModifiedDate;
 
       packages =
-        let
-          p = packageForVersions pkgs supportedPythonVersions;
-        in
-          p // { default = p.${packageName defaultPythonVersion}; };
+        withDefault
+          (packageForVersions extras supportedPythonVersions)
+          defaultPythonVersion;
 
       apps.default = {
         type = "app";
@@ -88,9 +50,8 @@
       };
 
       devShells =
-        let
-          s = devShellForVersions pkgs supportedPythonVersions;
-        in
-          s // { default = s.${packageName defaultPythonVersion}; };
+        withDefault
+          (devShellForVersions extras supportedPythonVersions)
+          defaultPythonVersion;
     });
 }
---
+++ lib.nix
@@ -0,0 +1,83 @@
+{
+# The name of the system for which to build, eg "x86_64-linux"
+  system
+# The nixpkgs to use
+, pkgs
+# The mach-nix (flake) to use
+, mach-nix
+}:
+# Below, wherever we talk about a Python version, we're talking about the name
+# of a Python package in nixpkgs - eg "python39".
+rec {
+  # Construct the name of the package for a given version of Python
+  # str -> str
+  packageName = pythonVersion: "tahoe-lafs-${pythonVersion}";
+
+  # Create a Tahoe-LAFS package using the given Python version and extras.
+  # str -> [str] -> derivation
+  packageForVersion = extras: pythonVersion:
+    pkgs.callPackage ./default.nix {
+      inherit extras pythonVersion;
+      inherit (mach-nix.lib.${system}) buildPythonPackage;
+    };
+
+  # Create packages for all of the given Python versions.
+  #
+  # [str] -> [str] -> setOf derivation
+  packageForVersions = extras: pythonVersions:
+    let
+      mkPackage = packageForVersion extras;
+    in
+      builtins.foldl' (accum: pyVersion: accum // {
+        ${packageName pyVersion} = mkPackage pyVersion;
+      }) {} pythonVersions;
+
+  # Compute the requirements string for Tahoe-LAFS with the given extras on
+  # the given Python version.
+  #
+  # [str] -> str -> str
+  requirementsForVersion = extras: pythonVersion: (
+    packageForVersion extras pythonVersion
+  ).requirements;
+
+  # Create a Python environment derivation using the given version of Python
+  # and that contains Tahoe-LAFS requirements (including those identified by
+  # the given extras).
+  #
+  # [str] -> str -> derivation
+  devPy = extras: pythonVersion:
+    mach-nix.lib.${system}.mkPython {
+      inherit ((packageForVersion extras pythonVersion).meta.mach-nix) providers _;
+      requirements = requirementsForVersion extras pythonVersion;
+    };
+
+  # Create a shell derivation for a development environment for Tahoe-LAFS for
+  # the given extras and Python version.  The resulting environment places the
+  # source from the checkout first in PYTHONPATH.
+  #
+  # [str] -> str -> derivation
+  devShellForVersion = extras: pythonVersion:
+    pkgs.mkShell {
+      shellHook = "export PYTHONPATH=\${PWD}/src:\${PYTHONPATH}";
+      buildInputs = [ (devPy extras pythonVersion) ];
+    };
+
+  # Create development environments for all of the given Python versions.  In
+  # addition to the given extras, the "test" extra is included.
+  #
+  # [str] -> [str] -> setOf derivation
+  devShellForVersions = extras: pythonVersions:
+    let
+      mkPackage = devShellForVersion (extras ++ [ "test" ]);
+    in
+      builtins.foldl' (accum: pyVersion: accum // {
+        ${packageName pyVersion} = mkPackage pyVersion;
+      }) {} pythonVersions;
+
+  # Create a set of derivations that includes a default that points at the
+  # entry for the given Python version.
+  #
+  # setOf derivations -> str -> setOf derivations
+  withDefault = packages: pythonVersion:
+    packages // { default = packages.${packageName pythonVersion}; };
+}
