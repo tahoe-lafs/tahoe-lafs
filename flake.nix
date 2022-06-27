0d5144842878070cd4610f0a5643c1fd28d2889c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-06-27 15:44:50 -0400

    promote pypi-deps-db to a top-level input for cli override

--- flake.nix
+++ flake.nix
@@ -4,20 +4,21 @@
     url = "github:NixOS/nixpkgs?rev=838eefb4f93f2306d4614aafb9b2375f315d917f";
   };
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pypi-deps-db = {
+    flake = false;
+    url = "github:DavHau/pypi-deps-db?rev=76b8f1e44a8ec051b853494bcf3cc8453a294a6a";
+  };
   inputs.mach-nix = {
     flake = true;
     url = "github:DavHau/mach-nix?rev=bdc97ba6b2ecd045a467b008cff4ae337b6a7a6b";
     inputs = {
-      pypi-deps-db = {
-        flake = false;
-        url = "github:DavHau/pypi-deps-db?rev=76b8f1e44a8ec051b853494bcf3cc8453a294a6a";
-      };
+      pypi-deps-db.follows = "pypi-deps-db";
       nixpkgs.follows = "nixpkgs";
       flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix }:
+  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
 
       lib = import ./nix/lib.nix {
