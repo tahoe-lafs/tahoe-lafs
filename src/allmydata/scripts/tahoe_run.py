0eeb11c9cd45598fbe2e5bdccb4f9cf50fe222f3
meejah <meejah@meejah.ca>
2022-09-20 14:44:51 -0600

    after shutdown

--- src/allmydata/scripts/tahoe_run.py
+++ src/allmydata/scripts/tahoe_run.py
@@ -271,7 +271,7 @@ def run(reactor, config, runApp=twistd.runApp):
         return 1
     else:
         reactor.addSystemEventTrigger(
-            "during", "shutdown",
+            "after", "shutdown",
             lambda: cleanup_pidfile(pidfile)
         )
 
