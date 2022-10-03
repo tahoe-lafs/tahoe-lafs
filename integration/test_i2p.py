a53420c1931b4ec9c6a40f5105a44d7d4ac0f846
Itamar Turner-Trauring <itamar@pythonspeed.com>
2022-10-03 10:49:01 -0400

    Use known working version of i2pd.

--- integration/test_i2p.py
+++ integration/test_i2p.py
@@ -55,7 +55,7 @@ def i2p_network(reactor, temp_dir, request):
         proto,
         which("docker"),
         (
-            "docker", "run", "-p", "7656:7656", "purplei2p/i2pd",
+            "docker", "run", "-p", "7656:7656", "purplei2p/i2pd:release-2.43.0",
             # Bad URL for reseeds, so it can't talk to other routers.
             "--reseed.urls", "http://localhost:1/",
         ),
