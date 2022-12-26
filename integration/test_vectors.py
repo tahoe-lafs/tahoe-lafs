4a39c4b7ecd5c11cbe35dd74172bac0c65b97167
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-12-26 17:08:30 -0500

    Add SDMF and MDMF

--- integration/test_vectors.py
+++ integration/test_vectors.py
@@ -4,10 +4,13 @@ Verify certain results against test vectors with well-known results.
 
 from __future__ import annotations
 
+from time import sleep
 from typing import AsyncGenerator, Iterator
 from hashlib import sha256
 from itertools import product
 from yaml import safe_dump
+
+from attrs import frozen
 
 from pytest import mark
 from pytest_twisted import ensureDeferred
@@ -21,6 +24,10 @@ def digest(bs: bytes) -> bytes:
 
 def hexdigest(bs: bytes) -> str:
     return sha256(bs).hexdigest()
+
+
+# Sometimes upload fail spuriously...
+RETRIES = 3
 
 
 # Just a couple convergence secrets.  The only thing we do with this value is
@@ -58,13 +65,34 @@ OBJECT_DESCRIPTIONS = [
     (digest(b"barbaz"), SEGMENT_SIZE * 64 + 1),
 ]
 
+# CHK have a max of 256 shares.  SDMF / MDMF have a max of 255 shares!
+# Represent max symbolically and resolve it when we know what format we're
+# dealing with.
+MAX_SHARES = "max"
+
+# SDMF and MDMF encode share counts (N and k) into the share itself as an
+# unsigned byte.  They could have encoded (share count - 1) to fit the full
+# range supported by ZFEC into the unsigned byte - but they don't.  So 256 is
+# inaccessible to those formats and we set the upper bound at 255.
+MAX_SHARES_MAP = {
+    "chk": 256,
+    "sdmf": 255,
+    "mdmf": 255,
+}
+
 ZFEC_PARAMS = [
     (1, 1),
     (1, 3),
     (2, 3),
     (3, 10),
     (71, 255),
-    (101, 256),
+    (101, MAX_SHARES),
+]
+
+FORMATS = [
+    "chk",
+    "sdmf",
+    "mdmf",
 ]
 
 @mark.parametrize('convergence_idx', range(len(CONVERGENCE_SECRETS)))
@@ -80,27 +108,29 @@ def test_convergence(convergence_idx):
 @mark.parametrize('params_idx', range(len(ZFEC_PARAMS)))
 @mark.parametrize('convergence_idx', range(len(CONVERGENCE_SECRETS)))
 @mark.parametrize('data_idx', range(len(OBJECT_DESCRIPTIONS)))
+@mark.parametrize('fmt_idx', range(len(FORMATS)))
 @ensureDeferred
-async def test_chk_capability(reactor, request, alice, params_idx, convergence_idx, data_idx):
+async def test_capability(reactor, request, alice, params_idx, convergence_idx, data_idx, fmt_idx):
     """
-    The CHK capability that results from uploading certain well-known data
+    The capability that results from uploading certain well-known data
     with certain well-known parameters results in exactly the previously
     computed value.
     """
-    key, params, convergence, data = load_case(
+    case = load_case(
         params_idx,
         convergence_idx,
         data_idx,
+        fmt_idx,
     )
 
     # rewrite alice's config to match params and convergence
-    await reconfigure(reactor, request, alice, (1,) + params, convergence)
+    await reconfigure(reactor, request, alice, (1,) + case.params, case.convergence)
 
-    # upload data as a CHK
-    actual = upload(alice, "chk", data)
+    # upload data in the correct format
+    actual = upload(alice, case.fmt, case.data)
 
     # compare the resulting cap to the expected result
-    expected = vectors.chk[key]
+    expected = vectors.capabilities[case.key]
     assert actual == expected
 
 
@@ -119,6 +149,7 @@ async def test_generate(reactor, request, alice):
         range(len(ZFEC_PARAMS)),
         range(len(CONVERGENCE_SECRETS)),
         range(len(OBJECT_DESCRIPTIONS)),
+        range(len(FORMATS)),
     )
     results = await asyncfoldr(
         generate(reactor, request, alice, space),
@@ -132,6 +163,7 @@ async def test_generate(reactor, request, alice):
                 "zfec": ZFEC_PARAMS,
                 "convergence": CONVERGENCE_SECRETS,
                 "objects": OBJECT_DESCRIPTIONS,
+                "formats": FORMATS,
             },
             "vector": results,
         }))
@@ -141,7 +173,7 @@ async def generate(
         reactor,
         request,
         alice: TahoeProcess,
-        space: Iterator[int, int, int],
+        space: Iterator[int, int, int, int],
 ) -> AsyncGenerator[tuple[str, str], None]:
     """
     Generate all of the test vectors using the given node.
@@ -152,7 +184,13 @@ async def generate(
     :param request: The pytest request object to use to arrange process
         cleanup.
 
+    :param format: The name of the encryption/data format to use.
+
     :param alice: The Tahoe-LAFS node to use to generate the test vectors.
+
+    :param space: An iterator of coordinates in the test vector space for
+       which to generate values.  The elements of each tuple give indexes into
+       ZFEC_PARAMS, CONVERGENCE_SECRETS, OBJECT_DESCRIPTIONS, and FORMTS.
 
     :return: The yield values are two-tuples describing a test vector.  The
         first element is a string describing a case and the second element is
@@ -162,16 +200,17 @@ async def generate(
     # reliability, be happy if we can put shares anywhere
     happy = 1
     node_key = (None, None)
-    for params_idx, secret_idx, data_idx in space:
-        key, params, secret, data = load_case(params_idx, secret_idx, data_idx)
-        if node_key != (params, secret):
-            await reconfigure(reactor, request, alice, (happy,) + params, secret)
-            node_key = (params, secret)
+    for params_idx, secret_idx, data_idx, fmt_idx in space:
+        case = load_case(params_idx, secret_idx, data_idx, fmt_idx)
+        if node_key != (case.params, case.convergence):
+            await reconfigure(reactor, request, alice, (happy,) + case.params, case.convergence)
+            node_key = (case.params, case.convergence)
 
-        yield key, upload(alice, "chk", data)
+        cap = upload(alice, case.fmt, case.data)
+        yield case.key, cap
 
 
-def key(params: int, secret: int, data: int) -> str:
+def key(params: int, secret: int, data: int, fmt: int) -> str:
     """
     Construct the key describing the case defined by the given parameters.
 
@@ -179,7 +218,7 @@ def key(params: int, secret: int, data: int) -> str:
 
     :return: A distinct string for the given inputs.
     """
-    return f"{params}-{secret}-{data}"
+    return f"{params}-{secret}-{data}-{fmt}"
 
 
 def stretch(seed: bytes, size: int) -> bytes:
@@ -196,9 +235,32 @@ def stretch(seed: bytes, size: int) -> bytes:
     return (seed * multiples)[:size]
 
 
-def load_case(params_idx: int, convergence_idx: int, data_idx: int) -> tuple[str, tuple[int, int], bytes, bytes]:
+def load_case(
+        params_idx: int,
+        convergence_idx: int,
+        data_idx: int,
+        fmt_idx: int
+) -> Case:
+    """
+    :return:
+    """
     params = ZFEC_PARAMS[params_idx]
+    fmt = FORMATS[fmt_idx]
     convergence = CONVERGENCE_SECRETS[convergence_idx]
     data = stretch(*OBJECT_DESCRIPTIONS[data_idx])
-    key = (params_idx, convergence_idx, data_idx)
-    return (key, params, convergence, data)
+    if params[1] == MAX_SHARES:
+        params = (params[0], MAX_SHARES_MAP[fmt])
+    k = key(params_idx, convergence_idx, data_idx, fmt_idx)
+    return Case(k, fmt, params, convergence, data)
+
+
+@frozen
+class Case:
+    """
+    Represent one case for which we want/have a test vector.
+    """
+    key: str
+    fmt: str
+    params: tuple[int, int]
+    convergence: bytes
+    data: bytes
