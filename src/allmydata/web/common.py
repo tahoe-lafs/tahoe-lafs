3ce5ee6f0304131a5cc4ebbbed2237842084f3d2
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-01-07 07:17:40 -0500

    get Literal from somewhere it is more likely to be

--- src/allmydata/web/common.py
+++ src/allmydata/web/common.py
@@ -5,7 +5,8 @@ from __future__ import annotations
 
 from six import ensure_str
 
-from typing import Optional, Union, TypeVar, Literal, overload
+from typing import Optional, Union, TypeVar, overload
+from typing_extensions import Literal
 
 import time
 import json
