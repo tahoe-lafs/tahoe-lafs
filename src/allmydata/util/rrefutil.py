230a9fad55bde401c3c1a94c75f8902c1dc0c8e5
dlee <daniel.zwlee@gmail.com>
2023-03-08 16:39:12 -0600

    Type annotations added for rrefutil.py. Note rref typed as Any not foolscpa.referenceable.RemoteReference to add class variable rref.version which goes against static typing

--- src/allmydata/util/rrefutil.py
+++ src/allmydata/util/rrefutil.py
@@ -11,18 +11,25 @@ if PY2:
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 
 from foolscap.api import Violation, RemoteException
+from twisted.internet.defer import Deferred
+from twisted.python.failure import Failure
+from typing import Any
 
-
-def add_version_to_remote_reference(rref, default):
+# Type rref: RemoteReference from foolscap.referenceable
+# but since we are dynamically adding a class variable
+# it kind of defeats the purpose of static typing so leave as 'Any'
+def add_version_to_remote_reference(rref: Any, default: Any) -> Deferred[Any]:
     """I try to add a .version attribute to the given RemoteReference. I call
     the remote get_version() method to learn its version. I'll add the
     default value if the remote side doesn't appear to have a get_version()
     method."""
-    d = rref.callRemote("get_version")
-    def _got_version(version):
+    if not hasattr(rref, 'version'):
+        rref.version =
+    d: Deferred[Any] = rref.callRemote("get_version")
+    def _got_version(version: Any) -> Any:
         rref.version = version
         return rref
-    def _no_get_version(f):
+    def _no_get_version(f: Failure) -> Any:
         f.trap(Violation, RemoteException)
         rref.version = default
         return rref
