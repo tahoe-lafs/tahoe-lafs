505032d0cac83111341304335c88d2ee95afdd9f
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-03-14 20:38:46 -0400

    a note about what this is and what's going on upstream

--- nix/pycddl.nix
+++ nix/pycddl.nix
@@ -1,3 +1,10 @@
+# package https://gitlab.com/tahoe-lafs/pycddl
+#
+# also in the process of being pushed upstream
+# https://github.com/NixOS/nixpkgs/pull/221220
+#
+# we should switch to the upstream package when it is available from our
+# minimum version of nixpkgs
 { lib, fetchPypi, buildPythonPackage, rustPlatform }:
 buildPythonPackage rec {
   pname = "pycddl";
