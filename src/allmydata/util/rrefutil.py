e0362dc5600595bef14ea1b0f11679c6a623822b
dlee <daniel.zwlee@gmail.com>
2023-03-15 15:17:36 -0500

    Implemented typing protocol in rrefutil.py

--- src/allmydata/util/rrefutil.py
+++ src/allmydata/util/rrefutil.py
@@ -13,21 +13,23 @@ if PY2:
 from foolscap.api import Violation, RemoteException
 from twisted.internet.defer import Deferred
 from twisted.python.failure import Failure
-from typing import Any
+from typing import Protocol, Any, Dict
 
-# Type rref: RemoteReference from foolscap.referenceable
-# but since we are dynamically adding a class variable
-# it kind of defeats the purpose of static typing so leave as 'Any'
-def add_version_to_remote_reference(rref: Any, default: Any) -> Deferred[Any]:
+class Versioned(Protocol):
+    version: str
+    def callRemote(self, name: str, *args: Any, **kwargs: Dict[str, Any]) -> Deferred[Any]:
+        ...
+
+def add_version_to_remote_reference(rref: Versioned, default: Any) -> Deferred[Any]:
     """I try to add a .version attribute to the given RemoteReference. I call
     the remote get_version() method to learn its version. I'll add the
     default value if the remote side doesn't appear to have a get_version()
     method."""
     d: Deferred[Any] = rref.callRemote("get_version")
-    def _got_version(version: Any) -> Any:
+    def _got_version(version: str) -> Versioned:
         rref.version = version
         return rref
-    def _no_get_version(f: Failure) -> Any:
+    def _no_get_version(f: Failure) -> Versioned:
         f.trap(Violation, RemoteException)
         rref.version = default
         return rref
