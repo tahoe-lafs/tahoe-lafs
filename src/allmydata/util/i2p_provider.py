cbfbfe8b1e3075b5c131f93c3c9e5b02b03b2ac1
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-03-28 16:30:55 -0400

    top-of-file cleanups

--- src/allmydata/util/i2p_provider.py
+++ src/allmydata/util/i2p_provider.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Ported to Python 3.
-"""
-from __future__ import absolute_import, print_function, with_statement
-from __future__ import division
-from __future__ import unicode_literals
 
-from future.utils import PY2
-if PY2:
-    from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
+from __future__ import annotations
 
 import os
 
