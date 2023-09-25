130160e5a0adbda0f141259b8bd66bb57a2d19b1
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-09-25 14:28:27 -0400

    Drop psutil

--- setup.py
+++ setup.py
@@ -436,7 +436,6 @@ setup(name="tahoe-lafs", # also set in __init__.py
               "pytest-timeout",
               # Does our OpenMetrics endpoint adhere to the spec:
               "prometheus-client == 0.11.0",
-              "psutil",
           ] + tor_requires + i2p_requires,
           "tor": tor_requires,
           "i2p": i2p_requires,
