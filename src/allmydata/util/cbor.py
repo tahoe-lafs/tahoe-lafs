f0ce6e9d688d84622cb255255a0db75cb6d57c12
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-01-24 08:05:29 -0500

    Use Python version of CBOR loading.

--- src/allmydata/util/cbor.py
+++ src/allmydata/util/cbor.py
@@ -2,6 +2,20 @@
 Unified entry point for CBOR encoding and decoding.
 """
 
-from cbor2 import dumps, loads, dump, load
+import sys
+
+# We don't want to use the C extension for loading, at least for now, but using
+# it for dumping should be fine.
+from cbor2 import dumps, dump
+
+# Now, override the C extension so we can import the Python versions of loading
+# functions.
+del sys.modules["cbor2"]
+sys.modules["_cbor2"] = None
+from cbor2 import load, loads
+
+# Quick validation that we got the Python version, not the C version.
+assert type(load) == type(lambda: None), repr(load)
+assert type(loads) == type(lambda: None), repr(loads)
 
 __all__ = ["dumps", "loads", "dump", "load"]
