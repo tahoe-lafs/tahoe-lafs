b7f4ade1b037585f33abc9b26ea6800c38a1f0da
meejah <meejah@meejah.ca>
2024-11-17 14:20:38 -0700

    debug

--- src/allmydata/immutable/downloader/fetcher.py
+++ src/allmydata/immutable/downloader/fetcher.py
@@ -63,7 +63,7 @@ class SegmentFetcher(object):
             self._cancel_all_requests()
             self._running = False
             # help GC ???
-            print("DEL", self)
+            print("DEL", self, hasattr(self, "_shares"))
             del self._shares, self._shares_from_server, self._active_share_map
             del self._share_observers
 
